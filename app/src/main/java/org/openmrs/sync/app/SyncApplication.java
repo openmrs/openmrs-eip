4c7d980de8021bf493b9b1056f4fb6ad432386e8
Wyclif Luyima <wyclif@openmrs.org>
2020-06-06 15:40:05 -0500

    Skip metadata tables when syncing

--- app/src/main/java/org/openmrs/sync/app/SyncApplication.java
+++ app/src/main/java/org/openmrs/sync/app/SyncApplication.java
@@ -36,6 +36,17 @@ public class SyncApplication {
 
     private CamelContext camelContext;
 
+    private final static Set<TableToSyncEnum> IGNORE_TABLES;
+
+    static {
+        IGNORE_TABLES = new HashSet();
+        IGNORE_TABLES.add(TableToSyncEnum.CONCEPT_ATTRIBUTE);
+        IGNORE_TABLES.add(TableToSyncEnum.LOCATION_ATTRIBUTE);
+        IGNORE_TABLES.add(TableToSyncEnum.PROVIDER_ATTRIBUTE);
+        IGNORE_TABLES.add(TableToSyncEnum.CONCEPT);
+        IGNORE_TABLES.add(TableToSyncEnum.LOCATION);
+    }
+
     public SyncApplication(@Nullable final ManagementDbInitImpl managementDbInit,
                            final CamelContext camelContext) {
         this.managementDbInit = managementDbInit;
@@ -83,6 +94,11 @@ public class SyncApplication {
         final String dbName = env.getProperty("debezium.db.name");
         Set<String> tables = new HashSet(TableToSyncEnum.values().length);
         for (TableToSyncEnum tableToSyncEnum : TableToSyncEnum.values()) {
+            //TODO Remove the enum values instead including services
+            if (IGNORE_TABLES.contains(tableToSyncEnum)) {
+                continue;
+            }
+
             tables.add(dbName + "." + tableToSyncEnum.name());
         }
 
