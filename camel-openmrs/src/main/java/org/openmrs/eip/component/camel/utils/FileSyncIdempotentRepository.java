684865a26ebc621d9cccbe6636d43f5c66e4fada
Wyclif Luyima <wyclif@openmrs.org>
2020-09-25 00:04:53 -0500

    Cleaned up log messages and removed stray code change

--- camel-openmrs/src/main/java/org/openmrs/eip/component/camel/utils/FileSyncIdempotentRepository.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/camel/utils/FileSyncIdempotentRepository.java
@@ -15,7 +15,7 @@ public class FileSyncIdempotentRepository extends FileIdempotentRepository {
 
     private static final String FILE_NAME = "/store";
 
-    public FileSyncIdempotentRepository(@Value("${camel.output.endpoint.file.locationw}") final String path) {
+    public FileSyncIdempotentRepository(@Value("${camel.output.endpoint.file.location}") final String path) {
         super(new File(path + FILE_NAME), new HashMap<>());
     }
 }
