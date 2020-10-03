75ba1a2e99eacdff56ec22c75ca5d65ba147bc55
Wyclif Luyima <wyclif@openmrs.org>
2020-10-03 17:00:46 -0400

    Added route for inbound-complex-obs-sync, deleted unused route and fixed predicate that checks if file sync to not fail for non file paylodds

--- camel-openmrs/src/main/java/org/openmrs/eip/component/camel/utils/IsFilePredicate.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/camel/utils/IsFilePredicate.java
@@ -13,9 +13,13 @@ public class IsFilePredicate implements Predicate {
 
     @Override
     public boolean matches(final Exchange exchange) {
-        String body = (String) exchange.getIn().getBody();
+        Object in = exchange.getIn().getBody();
+        if(in instanceof String){
+            String body = (String) in;
 
-        return body.startsWith(TypeEnum.FILE.getOpeningTag()) &&
-                body.endsWith(TypeEnum.FILE.getClosingTag());
+            return body.startsWith(TypeEnum.FILE.getOpeningTag()) && body.endsWith(TypeEnum.FILE.getClosingTag());
+        }
+
+        return false;
     }
 }
