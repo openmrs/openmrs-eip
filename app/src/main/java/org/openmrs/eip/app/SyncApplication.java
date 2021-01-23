39b5cf928c407c5b35ed739375b931216592f19c
Wyclif Luyima <wyclif@openmrs.org>
2021-01-23 00:36:49 -0600

    Moved common spring configuration to a publisher and removed redundant code

--- app/src/main/java/org/openmrs/eip/app/SyncApplication.java
+++ app/src/main/java/org/openmrs/eip/app/SyncApplication.java
@@ -1,12 +1,10 @@
 package org.openmrs.eip.app;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.PostConstruct;
 import javax.persistence.EntityManagerFactory;
 
 import org.apache.camel.CamelContext;
@@ -15,7 +13,6 @@ import org.apache.camel.builder.NoErrorHandlerBuilder;
 import org.apache.camel.processor.idempotent.jpa.JpaMessageIdRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.openmrs.eip.component.SyncProfiles;
-import org.openmrs.eip.component.camel.StringToLocalDateTimeConverter;
 import org.openmrs.eip.component.service.TableToSyncEnum;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
@@ -31,41 +28,12 @@ public class SyncApplication {
 	
 	private CamelContext camelContext;
 	
-	private final static Set<TableToSyncEnum> IGNORE_TABLES;
-	
-	static {
-		IGNORE_TABLES = new HashSet();
-		IGNORE_TABLES.add(TableToSyncEnum.CONCEPT_ATTRIBUTE);
-		IGNORE_TABLES.add(TableToSyncEnum.LOCATION_ATTRIBUTE);
-		IGNORE_TABLES.add(TableToSyncEnum.PROVIDER_ATTRIBUTE);
-		IGNORE_TABLES.add(TableToSyncEnum.CONCEPT);
-		IGNORE_TABLES.add(TableToSyncEnum.LOCATION);
-	}
-	
 	public SyncApplication(final CamelContext camelContext) {
 		this.camelContext = camelContext;
 	}
 	
 	public static void main(final String[] args) {
 		SpringApplication.run(SyncApplication.class, args);
-	}
-	
-	@PostConstruct
-	private void addTypeConverter() {
-		camelContext.getTypeConverterRegistry().addTypeConverter(LocalDateTime.class, String.class,
-		    new StringToLocalDateTimeConverter());
-	}
-	
-	/**
-	 * Bean to handle messages in error and re-route them to another route
-	 *
-	 * @return deadLetterChannelBuilder
-	 */
-	@Bean
-	public DeadLetterChannelBuilder deadLetterChannelBuilder() {
-		DeadLetterChannelBuilder builder = new DeadLetterChannelBuilder("direct:dlc");
-		builder.setUseOriginalMessage(true);
-		return builder;
 	}
 	
 	@Bean("outBoundErrorHandler")
@@ -91,29 +59,6 @@ public class SyncApplication {
 	@Profile(SyncProfiles.SENDER)
 	public JpaMessageIdRepository getJpaIdempotentRepository(@Qualifier("mngtEntityManager") EntityManagerFactory emf) {
 		return new JpaMessageIdRepository(emf, "complexObsProcessor");
-	}
-	
-	@Bean
-	@Profile(SyncProfiles.SENDER)
-	public PropertySource getCustomPropertySource(ConfigurableEnvironment env) {
-		//Custom PropertySource that we can dynamically populate with generated property values which
-		//is not possible via the properties file e.g. to specify names of tables to sync.
-		final String dbName = env.getProperty("openmrs.db.name");
-		Set<String> tables = new HashSet(TableToSyncEnum.values().length);
-		for (TableToSyncEnum tableToSyncEnum : TableToSyncEnum.values()) {
-			//TODO Remove the enum values instead including services
-			if (IGNORE_TABLES.contains(tableToSyncEnum)) {
-				continue;
-			}
-			
-			tables.add(dbName + "." + tableToSyncEnum.name());
-		}
-		
-		Map<String, Object> props = Collections.singletonMap("debezium.tablesToSync", StringUtils.join(tables, ","));
-		PropertySource customPropSource = new MapPropertySource("custom", props);
-		env.getPropertySources().addLast(customPropSource);
-		
-		return customPropSource;
 	}
 	
 }
---
+++ publisher/src/main/java/org/openmrs/eip/publisher/config/CommonConfig.java
@@ -0,0 +1,80 @@
+package org.openmrs.eip.publisher.config;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.camel.builder.DeadLetterChannelBuilder;
+import org.apache.camel.component.jpa.JpaComponent;
+import org.apache.commons.lang3.StringUtils;
+import org.openmrs.eip.component.SyncProfiles;
+import org.openmrs.eip.component.service.TableToSyncEnum;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+public class CommonConfig {
+	
+	private final static Set<TableToSyncEnum> IGNORE_TABLES;
+	
+	static {
+		IGNORE_TABLES = new HashSet();
+		IGNORE_TABLES.add(TableToSyncEnum.CONCEPT_ATTRIBUTE);
+		IGNORE_TABLES.add(TableToSyncEnum.LOCATION_ATTRIBUTE);
+		IGNORE_TABLES.add(TableToSyncEnum.PROVIDER_ATTRIBUTE);
+		IGNORE_TABLES.add(TableToSyncEnum.CONCEPT);
+		IGNORE_TABLES.add(TableToSyncEnum.LOCATION);
+	}
+	
+	/**
+	 * Bean to handle messages in error and re-route them to another route
+	 *
+	 * @return deadLetterChannelBuilder
+	 */
+	@Bean
+	public DeadLetterChannelBuilder deadLetterChannelBuilder() {
+		DeadLetterChannelBuilder builder = new DeadLetterChannelBuilder("direct:dlc");
+		builder.setUseOriginalMessage(true);
+		return builder;
+	}
+	
+	@Bean(value = "jpa")
+	public JpaComponent jpa(@Qualifier(value = "mngtEntityManager") EntityManagerFactory entityManagerFactory) {
+		JpaComponent comp = new JpaComponent();
+		comp.setEntityManagerFactory(entityManagerFactory);
+		
+		return comp;
+	}
+	
+	@Bean
+	@Profile(SyncProfiles.SENDER)
+	public PropertySource getCustomPropertySource(ConfigurableEnvironment env) {
+		//Custom PropertySource that we can dynamically populate with generated property values which
+		//is not possible via the properties file e.g. to specify names of tables to sync.
+		final String dbName = env.getProperty("openmrs.db.name");
+		Set<String> tables = new HashSet(TableToSyncEnum.values().length);
+		for (TableToSyncEnum tableToSyncEnum : TableToSyncEnum.values()) {
+			//TODO Remove the enum values instead including services
+			if (IGNORE_TABLES.contains(tableToSyncEnum)) {
+				continue;
+			}
+			
+			tables.add(dbName + "." + tableToSyncEnum.name());
+		}
+		
+		Map<String, Object> props = Collections.singletonMap("debezium.tablesToSync", StringUtils.join(tables, ","));
+		PropertySource customPropSource = new MapPropertySource("custom", props);
+		env.getPropertySources().addLast(customPropSource);
+		
+		return customPropSource;
+	}
+	
+}
