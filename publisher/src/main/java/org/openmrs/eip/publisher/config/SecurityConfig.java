cab9513fec9523c6234c22ea7bdeb41f3fcb9e05
Wyclif Luyima <wyclif@openmrs.org>
2021-01-23 01:44:16 -0600

    Renamed SecurityConf to SecurityConfig

--- publisher/src/main/java/org/openmrs/eip/publisher/config/SecurityConfig.java
+++ publisher/src/main/java/org/openmrs/eip/publisher/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SecurityConf {
+public class SecurityConfig {
 	
 	@PostConstruct
 	private void addBCProvider() {
