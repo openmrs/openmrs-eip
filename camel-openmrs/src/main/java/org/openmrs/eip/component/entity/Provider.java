e4414bb336b901e0c02fd690f8a63e323b3f2383
jpboane <jpboane@gmail.com>
2021-02-11 09:15:19 +0200

    Resolved issue on Provider

--- camel-openmrs/src/main/java/org/openmrs/eip/component/entity/Provider.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/entity/Provider.java
@@ -1,5 +1,9 @@
 package org.openmrs.eip.component.entity;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,10 +11,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import org.openmrs.eip.component.entity.light.ConceptLight;
 import org.openmrs.eip.component.entity.light.PersonLight;
 import org.openmrs.eip.component.entity.light.ProviderManagementProviderRoleLight;
+import org.openmrs.eip.component.entity.light.UserLight;
+import org.openmrs.eip.component.utils.DateUtils;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,8 +27,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Table(name = "provider")
 @AttributeOverride(name = "id", column = @Column(name = "provider_id"))
-public class Provider extends BaseChangeableMetaDataEntity {
-	
+public class Provider extends BaseCreatableEntity {
 	@Column(name = "name")
     private String name;
 	 
@@ -43,4 +49,34 @@ public class Provider extends BaseChangeableMetaDataEntity {
     @OneToOne
     @JoinColumn(name = "person_id")
     private PersonLight person;
+    
+    @ManyToOne
+    @JoinColumn(name = "changed_by")
+    private UserLight changedBy;
+
+    @Column(name = "date_changed")
+    private LocalDateTime dateChanged;
+
+    @ManyToOne
+    @JoinColumn(name = "retired_by")
+    private UserLight retiredBy;
+
+    @Column(name = "date_retired")
+    private LocalDateTime dateRetired;
+
+    @Column(name = "retire_reason")
+    private String retireReason;
+
+    @NotNull
+    @Column(name = "retired")
+    private boolean retired;
+    
+    @Override
+    public boolean wasModifiedAfter(BaseEntity entity) {
+        BaseChangeableMetaDataEntity other = (BaseChangeableMetaDataEntity) entity;
+        List<LocalDateTime> dates = Arrays.asList(getDateChanged(), getDateRetired());
+        List<LocalDateTime> otherDates = Arrays.asList(other.getDateChanged(), other.getDateRetired());
+        return DateUtils.containsLatestDate(dates, otherDates);
+    }
+
 }
--- camel-openmrs/src/main/java/org/openmrs/eip/component/model/ProviderModel.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/model/ProviderModel.java
@@ -1,10 +1,11 @@
 package org.openmrs.eip.component.model;
 
+import java.time.LocalDateTime;
+
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
-public class ProviderModel extends BaseChangeableMetadataModel {
-	
+public class ProviderModel extends BaseModel {
     private String name;
 	 
     private String identifier;
@@ -16,6 +17,18 @@ public class ProviderModel extends BaseChangeableMetadataModel {
     private String specialityUuid;
 
     private String personUuid;
+
+    private boolean retired;
+
+    private String retiredByUuid;
+
+    private LocalDateTime dateRetired;
+
+    private String retiredReason;
+
+    private String changedByUuid;
+
+    private LocalDateTime dateChanged;
 
 	public String getName() {
 		return name;
@@ -31,14 +44,6 @@ public class ProviderModel extends BaseChangeableMetadataModel {
 
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
-	}
-
-	public String getPersonUuid() {
-		return personUuid;
-	}
-
-	public void setPersonUuid(String personUuid) {
-		this.personUuid = personUuid;
 	}
 
 	public String getProviderRoleUuid() {
@@ -64,4 +69,61 @@ public class ProviderModel extends BaseChangeableMetadataModel {
 	public void setSpecialityUuid(String specialityUuid) {
 		this.specialityUuid = specialityUuid;
 	}
+
+	public String getPersonUuid() {
+		return personUuid;
+	}
+
+	public void setPersonUuid(String personUuid) {
+		this.personUuid = personUuid;
+	}
+
+	public boolean isRetired() {
+		return retired;
+	}
+
+	public void setRetired(boolean retired) {
+		this.retired = retired;
+	}
+
+	public String getRetiredByUuid() {
+		return retiredByUuid;
+	}
+
+	public void setRetiredByUuid(String retiredByUuid) {
+		this.retiredByUuid = retiredByUuid;
+	}
+
+	public LocalDateTime getDateRetired() {
+		return dateRetired;
+	}
+
+	public void setDateRetired(LocalDateTime dateRetired) {
+		this.dateRetired = dateRetired;
+	}
+
+	public String getRetiredReason() {
+		return retiredReason;
+	}
+
+	public void setRetiredReason(String retiredReason) {
+		this.retiredReason = retiredReason;
+	}
+
+	public String getChangedByUuid() {
+		return changedByUuid;
+	}
+
+	public void setChangedByUuid(String changedByUuid) {
+		this.changedByUuid = changedByUuid;
+	}
+
+	public LocalDateTime getDateChanged() {
+		return dateChanged;
+	}
+
+	public void setDateChanged(LocalDateTime dateChanged) {
+		this.dateChanged = dateChanged;
+	}
+
 }
