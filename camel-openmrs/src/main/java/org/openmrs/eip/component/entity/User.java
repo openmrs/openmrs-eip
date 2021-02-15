64268f27119f287d6376d9de6cb11c2b3dcd3f2c
jpboane <jpboane@gmail.com>
2021-02-15 14:43:06 +0200

    Changed User entity to extends BaseCreatableEntity

--- camel-openmrs/src/main/java/org/openmrs/eip/component/entity/User.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/entity/User.java
@@ -1,14 +1,21 @@
 package org.openmrs.eip.component.entity;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.openmrs.eip.component.entity.light.PersonLight;
+import org.openmrs.eip.component.entity.light.UserLight;
+import org.openmrs.eip.component.utils.DateUtils;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,7 +25,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Table(name = "users")
 @AttributeOverride(name = "id", column = @Column(name = "user_id"))
-public class User extends BaseChangeableMetaDataEntity {
+public class User extends BaseCreatableEntity {
 	
 	@NotNull
 	@Column(name = "system_id")
@@ -41,10 +48,38 @@ public class User extends BaseChangeableMetaDataEntity {
     @JoinColumn(name = "person_id")
     private PersonLight person;
     
-    
     @Column(name = "activation_key")
     private String activationKey;
     
     @Column(name = "email")
     private String email;
+    
+    @ManyToOne
+    @JoinColumn(name = "changed_by")
+    private UserLight changedBy;
+
+    @Column(name = "date_changed")
+    private LocalDateTime dateChanged;
+
+    @ManyToOne
+    @JoinColumn(name = "retired_by")
+    private UserLight retiredBy;
+
+    @Column(name = "date_retired")
+    private LocalDateTime dateRetired;
+
+    @Column(name = "retire_reason")
+    private String retireReason;
+
+    @NotNull
+    @Column(name = "retired")
+    private boolean retired;
+    
+    @Override
+    public boolean wasModifiedAfter(BaseEntity entity) {
+        BaseChangeableMetaDataEntity other = (BaseChangeableMetaDataEntity) entity;
+        List<LocalDateTime> dates = Arrays.asList(getDateChanged(), getDateRetired());
+        List<LocalDateTime> otherDates = Arrays.asList(other.getDateChanged(), other.getDateRetired());
+        return DateUtils.containsLatestDate(dates, otherDates);
+    }
 }
