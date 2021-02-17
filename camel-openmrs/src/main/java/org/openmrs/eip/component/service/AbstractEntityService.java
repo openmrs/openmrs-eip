14d665f596acc03add2564ce73542c91a8a3ed47
Wyclif Luyima <wyclif@openmrs.org>
2021-02-17 02:33:20 -0600

    Fixed syncing of patient

--- camel-openmrs/src/main/java/org/openmrs/eip/component/service/AbstractEntityService.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/service/AbstractEntityService.java
@@ -2,12 +2,25 @@ package org.openmrs.eip.component.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.openmrs.eip.component.entity.BaseEntity;
+import org.openmrs.eip.component.entity.Order;
+import org.openmrs.eip.component.entity.Patient;
 import org.openmrs.eip.component.exception.ConflictsFoundException;
+import org.openmrs.eip.component.exception.EIPException;
 import org.openmrs.eip.component.mapper.EntityToModelMapper;
 import org.openmrs.eip.component.mapper.ModelToEntityMapper;
 import org.openmrs.eip.component.model.BaseModel;
+import org.openmrs.eip.component.model.DrugOrderModel;
+import org.openmrs.eip.component.model.PatientModel;
+import org.openmrs.eip.component.model.TestOrderModel;
 import org.openmrs.eip.component.repository.SyncEntityRepository;
+import org.openmrs.eip.component.service.impl.AbstractSubclassEntityService;
+import org.openmrs.eip.component.service.light.AbstractLightService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +32,18 @@ public abstract class AbstractEntityService<E extends BaseEntity, M extends Base
     protected SyncEntityRepository<E> repository;
     protected EntityToModelMapper<E, M> entityToModelMapper;
     protected ModelToEntityMapper<M, E> modelToEntityMapper;
+
+    private static final String INSERT_PATIENT = "insert into patient (patient_id,creator,date_created,voided) " +
+            "values (?, ?, ?, ?)";
+
+    private static final String INSERT_TEST_ORDER = "insert into test_order (order_id) values (?)";
+
+    private static final String INSERT_DRUG_ORDER = "insert into drug_order (order_id,dispense_as_written) values (?, ?)";
+
+    private static final String GET_ORDER_ID = "select order_id from orders where uuid = (?)";
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
 
     public AbstractEntityService(final SyncEntityRepository<E> repository,
                                  final EntityToModelMapper<E, M> entityToModelMapper,
@@ -41,7 +66,34 @@ public abstract class AbstractEntityService<E extends BaseEntity, M extends Base
 
         E ety = modelToEntityMapper.apply(model);
 
-        M modelToReturn = model;
+        if ((etyInDb == null && model instanceof PatientModel) || (etyInDb instanceof Order &&
+                (model instanceof TestOrderModel || model instanceof DrugOrderModel))) {
+            //There is no row yet in the subclass table and we don't yet know the FK, get the parent row by uuid so
+            //we can get the id and set it on this subclass
+
+            Long id = null;
+            if (this instanceof AbstractSubclassEntityService) {
+                BaseEntity parent = ((AbstractSubclassEntityService) this).getParentRepository().findByUuid(model.getUuid());
+                if (parent != null) {
+                    id = parent.getId();
+                }
+
+                if (id == null) {
+                    //We have a problem
+                    throw new EIPException("No row found in tha parent table for: " + model);
+                }
+
+                log.info(model.getClass() + " has no matching row in the parent table, inserting one");
+
+                insertParentRow(ety, id, model);
+            } else {
+                id = etyInDb.getId();
+            }
+
+            ety.setId(id);
+        }
+
+        M modelToReturn;
 
         if (etyInDb == null) {
             modelToReturn = saveEntity(ety);
@@ -75,7 +127,7 @@ public abstract class AbstractEntityService<E extends BaseEntity, M extends Base
 
     @Override
     public M getModel(final String uuid) {
-        E entity  = repository.findByUuid(uuid);
+        E entity = repository.findByUuid(uuid);
         return entity != null ? entityToModelMapper.apply(entity) : null;
     }
 
@@ -105,4 +157,66 @@ public abstract class AbstractEntityService<E extends BaseEntity, M extends Base
     private String getMsg(final E ety, final String uuid, final String s) {
         return "Entity of type " + ety.getClass().getName() + " with uuid " + uuid + s;
     }
+
+    private void insertParentRow(E ety, Long id, BaseModel model) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = null;
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+
+            Query query;
+            if (ety instanceof Patient) {
+                query = em.createNativeQuery(INSERT_PATIENT);
+                query.setParameter(1, id);
+                query.setParameter(2, AbstractLightService.DEFAULT_USER_ID);
+                query.setParameter(3, AbstractLightService.DEFAULT_DATE);
+                query.setParameter(4, false);
+            } else if (model instanceof TestOrderModel) {
+                query = em.createNativeQuery(INSERT_TEST_ORDER);
+                query.setParameter(1, id);
+            } else if (model instanceof DrugOrderModel) {
+                query = em.createNativeQuery(INSERT_DRUG_ORDER);
+                query.setParameter(1, id);
+                query.setParameter(2, false);
+            } else {
+                throw new EIPException("Don't know how to sync: " + model);
+            }
+
+            query.executeUpdate();
+
+            tx.commit();
+        } catch (Exception e) {
+            log.info("Failed to insert row for subclass entity: " + ety);
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    private Long getOrderId(BaseModel model) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        try {
+            Query query = em.createNativeQuery(INSERT_TEST_ORDER);
+            query.setParameter(1, model.getUuid());
+            List<Object> matches = query.getResultList();
+            if (matches != null && matches.size() == 1) {
+                return (Long) matches.get(0);
+            } else {
+                throw new EIPException("Found multiple orders with uuid: " + model.getUuid());
+            }
+        } catch (Exception e) {
+            throw new EIPException("Failed to get order id for order: " + model);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
 }
