0ed32290fbd16980fe52bf9178047a85ced1dde2
Wyclif Luyima <wyclif@openmrs.org>
2021-03-09 11:30:12 -0600

    Added checkDigit and required properties to PatientIdentifierTypeLight

--- camel-openmrs/src/main/java/org/openmrs/eip/component/entity/light/PatientIdentifierTypeLight.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/entity/light/PatientIdentifierTypeLight.java
@@ -17,6 +17,13 @@ import javax.validation.constraints.NotNull;
 public class PatientIdentifierTypeLight extends RetireableLightEntity {
 
     @NotNull
-    @Column(name = "name")
     private String name;
+
+    @NotNull
+    private Boolean required;
+
+    @NotNull
+    @Column(name="check_digit")
+    private Boolean checkDigit;
+
 }
