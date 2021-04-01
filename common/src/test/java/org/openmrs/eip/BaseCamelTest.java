935934071a90e97aeecbdf521b859d367dd1a2c4
wluyima <wyclif@openmrs.org>
2021-04-01 17:25:58 -0500

    Cleaned up Logger in BaseCamelTest

--- common/src/test/java/org/openmrs/eip/BaseCamelTest.java
+++ common/src/test/java/org/openmrs/eip/BaseCamelTest.java
@@ -16,6 +16,7 @@ import org.apache.camel.test.spring.CamelSpringRunner;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,7 +37,7 @@ import ch.qos.logback.core.read.ListAppender;
 @TestExecutionListeners(value = DependencyInjectionTestExecutionListener.class)
 public abstract class BaseCamelTest {
 	
-	private static final org.slf4j.Logger log = LoggerFactory.getLogger(BaseCamelTest.class);
+	private static final Logger log = LoggerFactory.getLogger(BaseCamelTest.class);
 	
 	@Autowired
 	protected ApplicationContext applicationContext;
