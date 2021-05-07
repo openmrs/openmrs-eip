48534da25e81ee5452f8f2aff200ea0a5fa77c3d
wluyima <wyclif@openmrs.org>
2021-05-06 22:56:39 -0500

    Removed dbsync sender and receiver sub modules

--- distribution/activemq_setup/create_docker_network.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker network rm activemq-network
-docker network create --subnet 172.28.0.0/24 activemq-network
--- distribution/receiver/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar ${appJarFileName} -Dspring.profiles.active=receiver
