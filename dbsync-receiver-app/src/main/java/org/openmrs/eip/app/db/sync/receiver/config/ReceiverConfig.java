dbb3e05298bb61f0a3f90de55e53e1a7f23a955d
wluyima <wyclif@openmrs.org>
2021-05-11 22:06:27 -0500

    Updated artemis configuration for the receiver

--- dbsync-receiver-app/src/main/java/org/openmrs/eip/app/db/sync/receiver/config/ReceiverConfig.java
+++ dbsync-receiver-app/src/main/java/org/openmrs/eip/app/db/sync/receiver/config/ReceiverConfig.java
@@ -39,10 +39,13 @@ public class ReceiverConfig {
 	
 	@Bean("activeMqConnFactory")
 	public ConnectionFactory getConnectionFactory(Environment env) {
-		CachingConnectionFactory cf = new CachingConnectionFactory(new ActiveMQConnectionFactory());
-		cf.setClientId(env.getProperty("activemq.clientId"));
-		
-		return cf;
+        ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+        cf.setBrokerURL(env.getProperty("spring.artemis.brokerUrl"));
+        cf.setUserName(env.getProperty("spring.artemis.user"));
+        cf.setPassword(env.getProperty("spring.artemis.password"));
+        cf.setClientID(env.getProperty("activemq.clientId"));
+
+        return new CachingConnectionFactory(cf);
 	}
 	
 }
