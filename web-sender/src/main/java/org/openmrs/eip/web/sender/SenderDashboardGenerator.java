6348249008b679106cdd0ac487f28387d980faa4
wluyima <wyclif@openmrs.org>
2021-06-02 20:24:31 -0500

    EC-62 Implemented total count, most encountered error and updated lables on the sender dashboard

--- web-sender/src/main/java/org/openmrs/eip/web/sender/SenderDashboardGenerator.java
+++ web-sender/src/main/java/org/openmrs/eip/web/sender/SenderDashboardGenerator.java
@@ -4,10 +4,14 @@ import static org.apache.camel.impl.engine.DefaultFluentProducerTemplate.on;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.activemq.artemis.utils.collections.ConcurrentHashSet;
 import org.apache.camel.CamelContext;
 import org.openmrs.eip.app.AppUtils;
 import org.openmrs.eip.app.management.entity.SenderRetryQueueItem;
@@ -21,6 +25,13 @@ import org.springframework.stereotype.Component;
 public class SenderDashboardGenerator implements DashboardGenerator {
 	
 	private static final String ENTITY_NAME = SenderRetryQueueItem.class.getSimpleName();
+	
+	private static final Set<String> CONN_EX_CLASSES;
+	
+	static {
+		CONN_EX_CLASSES = new HashSet();
+		//CONN_EX_CLASSES.add()
+	}
 	
 	protected CamelContext camelContext;
 	
@@ -36,6 +47,10 @@ public class SenderDashboardGenerator implements DashboardGenerator {
 	public Dashboard generate() {
 		final Map<String, Map> tableStatsMap = new ConcurrentHashMap();
 		final Map exceptionCountMap = new ConcurrentHashMap();
+		final AtomicInteger totalErrorCount = new AtomicInteger();
+		final AtomicInteger connectionErrorCount = new AtomicInteger();
+		final AtomicInteger mostEncounteredErrorCount = new AtomicInteger();
+		final Set<Object> mostEncounteredErrors = new ConcurrentHashSet();
 		
 		AppUtils.getTablesToSync().parallelStream().forEach(table -> {
 			Arrays.stream(DatabaseOperation.values()).parallel().forEach(op -> {
@@ -45,6 +60,8 @@ public class SenderDashboardGenerator implements DashboardGenerator {
 				        .to("jpa:" + ENTITY_NAME + "?query=SELECT count(*) FROM " + ENTITY_NAME
 				                + " WHERE LOWER(event.tableName) = '" + tableName + "' AND event.operation = '" + op + "'")
 				        .request(Integer.class);
+				
+				totalErrorCount.addAndGet(count);
 				
 				if (count > 0) {
 					synchronized (this) {
@@ -63,12 +80,31 @@ public class SenderDashboardGenerator implements DashboardGenerator {
 			        + ENTITY_NAME + " GROUP BY exceptionType").request(List.class);
 			
 			items.parallelStream().forEach(values -> {
-				exceptionCountMap.put(values[0] != null ? values[0] : "uncategorized", values[1]);
+				final Object exception = values[0];
+				final Integer count = Integer.valueOf(values[1].toString());
+				exceptionCountMap.put(exception, count);
+				if (CONN_EX_CLASSES.contains(exception)) {
+					connectionErrorCount.addAndGet(count);
+				}
+				
+				synchronized (this) {
+					if (count >= mostEncounteredErrorCount.get()) {
+						if (count > mostEncounteredErrorCount.get()) {
+							mostEncounteredErrors.clear();
+						}
+						
+						mostEncounteredErrorCount.set(count);
+						mostEncounteredErrors.add(exception);
+					}
+				}
 			});
 		}
 		
 		Dashboard dashboard = new Dashboard();
-		Map<String, Map> errors = new ConcurrentHashMap(2);
+		Map<String, Object> errors = new ConcurrentHashMap(2);
+		errors.put("totalCount", totalErrorCount);
+		errors.put("connectionErrorCount", connectionErrorCount);
+		errors.put("mostEncounteredErrors", mostEncounteredErrors);
 		errors.put("tableStatsMap", tableStatsMap);
 		errors.put("exceptionCountMap", exceptionCountMap);
 		dashboard.add("errors", errors);
