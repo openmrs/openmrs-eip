d42c8f9a3bb177adecfa43ccab2fe8c86d471dca
wluyima <wyclif@openmrs.org>
2021-06-15 16:24:59 -0500

    EC-56 - Add web service endpoints for unprocessed DB events

---
+++ web-sender/src/main/java/org/openmrs/eip/web/sender/DebeziumEventController.java
@@ -0,0 +1,40 @@
+package org.openmrs.eip.web.sender;
+
+import java.util.Map;
+
+import org.openmrs.eip.app.management.entity.DebeziumEvent;
+import org.openmrs.eip.web.RestConstants;
+import org.openmrs.eip.web.contoller.BaseRestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(RestConstants.API_PATH + "/dbsync/sender/event")
+public class DebeziumEventController extends BaseRestController {
+	
+	@Override
+	public Class<?> getClazz() {
+		return DebeziumEvent.class;
+	}
+	
+	@GetMapping
+	public Map<String, Object> getAll() {
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching debezium events");
+		}
+		
+		return doGetAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Object get(@PathVariable("id") Integer id) {
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching debezium event with id: " + id);
+		}
+		
+		return doGet(id);
+	}
+	
+}
