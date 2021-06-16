1faaa190088fd352ca6ba42deaf74d07f4336957
wluyima <wyclif@openmrs.org>
2021-06-15 23:44:12 -0500

    EIP-16 Take into account class hierarchy when finding existing entity conflicts and errors
    EIP-16 Take into account class hierarchy when finding existing entity conflicts and errors

--- commons/src/main/java/org/openmrs/eip/Utils.java
+++ commons/src/main/java/org/openmrs/eip/Utils.java
@@ -1,7 +1,9 @@
 package org.openmrs.eip;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
 	
@@ -16,6 +18,40 @@ public class Utils {
 	 */
 	public static List<String> getWatchedTables() {
 		return Arrays.asList(WATCHED_TABLES);
+	}
+	
+	/**
+	 * Gets comma-separated list of table names surrounded with apostrophes associated to the specified
+	 * table as tables of subclass or superclass entities.
+	 *
+	 * @param tableName the tables to inspect
+	 * @return a list of table names
+	 */
+	public static List<String> getListOfTablesInHierarchy(String tableName) {
+		//TODO This logic should be extensible
+		List<String> tables = new ArrayList();
+		tables.add(tableName);
+		if ("person".equalsIgnoreCase(tableName) || "patient".equalsIgnoreCase(tableName)) {
+			tables.add("person".equalsIgnoreCase(tableName) ? "patient" : "person");
+		} else if ("orders".equalsIgnoreCase(tableName)) {
+			tables.add("test_order");
+			tables.add("drug_order");
+		} else if ("test_order".equalsIgnoreCase(tableName) || "drug_order".equalsIgnoreCase(tableName)) {
+			tables.add("orders");
+		}
+		
+		return tables;
+	}
+	
+	/**
+	 * Gets the tables associated to the specified table as tables of subclass or superclass entities.
+	 *
+	 * @param tableName the tables to inspect
+	 * @return a comma-separated list of table names
+	 */
+	public static String getTablesInHierarchy(String tableName) {
+		List<String> tables = getListOfTablesInHierarchy(tableName);
+		return String.join(",", tables.stream().map(tName -> "'" + tName + "'").collect(Collectors.toList()));
 	}
 	
 }
