8af9626b52d996a22d90280e6c28e248ec51c33a
wluyima <wyclif@openmrs.org>
2021-07-30 15:09:34 -0500

    EIP-33 Added actuator config that dy default disables all endpoints

---
+++ commons-web/src/main/java/org/openmrs/eip/web/ActuatorConfig.java
@@ -0,0 +1,8 @@
+package org.openmrs.eip.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application-common-web.properties")
+public class ActuatorConfig {}
