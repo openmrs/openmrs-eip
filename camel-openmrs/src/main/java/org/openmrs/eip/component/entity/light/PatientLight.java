96c3fbbb27137adf65542064f39e98fe460b4c6c
wluyima <wyclif@openmrs.org>
2021-09-09 23:33:48 -0500

    Backported - Fix creation of patient placeholders to mark them as voided

--- camel-openmrs/src/main/java/org/openmrs/eip/component/entity/light/PatientLight.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/entity/light/PatientLight.java
@@ -1,11 +1,15 @@
 package org.openmrs.eip.component.entity.light;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -13,14 +17,30 @@ import java.time.LocalDateTime;
 @Table(name = "patient")
 @PrimaryKeyJoinColumn(name = "patient_id")
 public class PatientLight extends PersonLight {
-
-    @NotNull
-    @Column(name = "allergy_status")
-    private String allergyStatus;
-
-    @Column(name = "creator")
-    private Long patientCreator;
-
-    @Column(name = "date_created")
-    private LocalDateTime patientDateCreated;
+	
+	@NotNull
+	@Column(name = "allergy_status")
+	private String allergyStatus;
+	
+	@NotNull
+	@Column(name = "creator")
+	private Long patientCreator;
+	
+	@NotNull
+	@Column(name = "date_created")
+	private LocalDateTime patientDateCreated;
+	
+	@NotNull
+	@Column(name = "voided")
+	private boolean patientVoided;
+	
+	@Column(name = "voided_by")
+	private Long patientVoidedBy;
+	
+	@Column(name = "date_voided")
+	private LocalDateTime patientDateVoided;
+	
+	@Column(name = "void_reason")
+	private String patientVoidReason;
+	
 }
--- camel-openmrs/src/main/java/org/openmrs/eip/component/service/light/AbstractLightService.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/service/light/AbstractLightService.java
@@ -1,6 +1,9 @@
 package org.openmrs.eip.component.service.light;
 
-import lombok.extern.slf4j.Slf4j;
+import java.lang.reflect.ParameterizedType;
+import java.time.LocalDateTime;
+import java.time.Month;
+
 import org.openmrs.eip.component.SyncContext;
 import org.openmrs.eip.component.entity.Person;
 import org.openmrs.eip.component.entity.light.LightEntity;
@@ -10,94 +13,105 @@ import org.openmrs.eip.component.repository.PersonRepository;
 import org.openmrs.eip.component.service.PatientServiceUtils;
 import org.openmrs.eip.component.utils.StringUtils;
 
-import java.lang.reflect.ParameterizedType;
-import java.time.LocalDateTime;
-import java.time.Month;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class AbstractLightService<E extends LightEntity> implements LightService<E> {
-
-    private static final String DEFAULT_UUID_PREFIX= "PLACEHOLDER_";
-    public static final String DEFAULT_STRING= "[Default]";
-    public static final LocalDateTime DEFAULT_DATE = LocalDateTime.of(1970, Month.JANUARY, 1, 0, 0);
-    public static final long DEFAULT_USER_ID = 1L;
-    public static final String DEFAULT_VOID_REASON= "[placeholder]";
-
-    protected OpenmrsRepository<E> repository;
-
-    public AbstractLightService(final OpenmrsRepository<E> repository) {
-        this.repository = repository;
-    }
-
-    /**
-     * Creates a placeholder entity with only mandatory attributes which will be
-     * unique for all entities with the same type.
-     * After a round of synchronization no placeholder entity should be left in the db
-     * @return the entity
-     */
-    protected abstract E createPlaceholderEntity(String uuid);
-
-    /**
-     * Get the placeholder uuid
-     * @return uuid
-     */
-    private  String getPlaceholderUuid() {
-        Class<E> persistentClass = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass())
-                .getActualTypeArguments()[0];
-
-        return DEFAULT_UUID_PREFIX + StringUtils.fromCamelCaseToSnakeCase(persistentClass.getSimpleName());
-    }
-
-    @Override
-    public E getOrInitEntity(final String uuid) {
-        return getOrInit(uuid);
-    }
-
-    @Override
-    public E getOrInitPlaceholderEntity() {
-        return getOrInit(getPlaceholderUuid());
-    }
-
-    private E getOrInit(final String uuid) {
-        if (uuid == null) {
-            return null;
-        }
-
-        E entity = repository.findByUuid(uuid);
-
-        if (entity == null) {
-            entity = createPlaceholderEntity(uuid);
-
-            entity.setUuid(uuid);
-            voidPlaceholder(entity);
-
-            if (entity instanceof PatientLight) {
-                //There is no row yet in the patient table and we don't yet know the FK, get the person row by uuid so
-                //we can get the id and set it on this subclass, but we also need to insert the patient row.
-                Long id;
-                Person person = SyncContext.getBean(PersonRepository.class).findByUuid(uuid);
-                if (person != null) {
-                    log.info("No matching row in the patient table, inserting one");
-                    id = person.getId();
-                    PatientLight p = (PatientLight) entity;
-                    Long creatorId = p.getPatientCreator() != null ? p.getPatientCreator() : DEFAULT_USER_ID;
-
-                    PatientServiceUtils.createPatient(id, uuid, p.isVoided(), creatorId, p.getPatientDateCreated());
-
-                    entity.setId(id);
-                }
-            }
-
-            entity = repository.save(entity);
-        }
-
-        return entity;
-    }
-
-    private void voidPlaceholder(final E entity) {
-        entity.setMuted(true);
-        entity.setMuteReason(DEFAULT_VOID_REASON);
-        entity.setDateMuted(DEFAULT_DATE);
-        entity.setMutedBy(DEFAULT_USER_ID);
-    }
+	
+	private static final String DEFAULT_UUID_PREFIX = "PLACEHOLDER_";
+	
+	public static final String DEFAULT_STRING = "[Default]";
+	
+	public static final LocalDateTime DEFAULT_DATE = LocalDateTime.of(1970, Month.JANUARY, 1, 0, 0);
+	
+	public static final long DEFAULT_USER_ID = 1L;
+	
+	public static final String DEFAULT_VOID_REASON = "[placeholder]";
+	
+	protected OpenmrsRepository<E> repository;
+	
+	public AbstractLightService(final OpenmrsRepository<E> repository) {
+		this.repository = repository;
+	}
+	
+	/**
+	 * Creates a placeholder entity with only mandatory attributes which will be unique for all entities
+	 * with the same type. After a round of synchronization no placeholder entity should be left in the
+	 * db
+	 * 
+	 * @return the entity
+	 */
+	protected abstract E createPlaceholderEntity(String uuid);
+	
+	/**
+	 * Get the placeholder uuid
+	 * 
+	 * @return uuid
+	 */
+	private String getPlaceholderUuid() {
+		Class<E> persistentClass = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass())
+		        .getActualTypeArguments()[0];
+		
+		return DEFAULT_UUID_PREFIX + StringUtils.fromCamelCaseToSnakeCase(persistentClass.getSimpleName());
+	}
+	
+	@Override
+	public E getOrInitEntity(final String uuid) {
+		return getOrInit(uuid);
+	}
+	
+	@Override
+	public E getOrInitPlaceholderEntity() {
+		return getOrInit(getPlaceholderUuid());
+	}
+	
+	private E getOrInit(final String uuid) {
+		if (uuid == null) {
+			return null;
+		}
+		
+		E entity = repository.findByUuid(uuid);
+		
+		if (entity == null) {
+			entity = createPlaceholderEntity(uuid);
+			
+			entity.setUuid(uuid);
+			voidPlaceholder(entity);
+			
+			if (entity instanceof PatientLight) {
+				//There is no row yet in the patient table and we don't yet know the FK, get the person row by uuid so
+				//we can get the id and set it on this subclass, but we also need to insert the patient row.
+				Long id;
+				Person person = SyncContext.getBean(PersonRepository.class).findByUuid(uuid);
+				if (person != null) {
+					log.info("No matching row in the patient table, inserting one");
+					id = person.getId();
+					PatientLight p = (PatientLight) entity;
+					Long creatorId = p.getPatientCreator() != null ? p.getPatientCreator() : DEFAULT_USER_ID;
+					
+					PatientServiceUtils.createPatient(id, uuid, p.isVoided(), creatorId, p.getPatientDateCreated());
+					
+					entity.setId(id);
+				}
+			}
+			
+			entity = repository.save(entity);
+		}
+		
+		return entity;
+	}
+	
+	private void voidPlaceholder(final E entity) {
+		entity.setMuted(true);
+		entity.setMuteReason(DEFAULT_VOID_REASON);
+		entity.setDateMuted(DEFAULT_DATE);
+		entity.setMutedBy(DEFAULT_USER_ID);
+		if (entity instanceof PatientLight) {
+			PatientLight patientLight = (PatientLight) entity;
+			patientLight.setPatientVoided(true);
+			patientLight.setPatientVoidedBy(DEFAULT_USER_ID);
+			patientLight.setPatientDateVoided(DEFAULT_DATE);
+			patientLight.setPatientVoidReason(DEFAULT_VOID_REASON);
+		}
+	}
 }
