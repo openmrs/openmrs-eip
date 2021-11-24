866cc71e21519eb74f6e7acc7e51cf9134d948d1
wluyima <wyclif@openmrs.org>
2021-11-23 21:31:35 -0600

    Added missing Person.causeOfDeathNonCoded property

--- camel-openmrs/src/main/java/org/openmrs/eip/component/entity/Person.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/entity/Person.java
@@ -1,13 +1,22 @@
 package org.openmrs.eip.component.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.openmrs.eip.component.entity.light.ConceptLight;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.openmrs.eip.component.entity.light.ConceptLight;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -16,32 +25,35 @@ import java.time.LocalTime;
 @Inheritance(strategy = InheritanceType.JOINED)
 @AttributeOverride(name = "id", column = @Column(name = "person_id"))
 public class Person extends BaseChangeableDataEntity {
-
-    @Column(name = "gender")
-    private String gender;
-
-    @Column(name = "birthdate")
-    private LocalDate birthdate;
-
-    @NotNull
-    @Column(name = "birthdate_estimated")
-    private boolean birthdateEstimated;
-
-    @NotNull
-    @Column(name = "dead")
-    private boolean dead;
-
-    @Column(name = "death_date")
-    private LocalDate deathDate;
-
-    @ManyToOne
-    @JoinColumn(name = "cause_of_death")
-    private ConceptLight causeOfDeath;
-
-    @NotNull
-    @Column(name = "deathdate_estimated")
-    private boolean deathdateEstimated;
-
-    @Column(name = "birthtime")
-    private LocalTime birthtime;
+	
+	@Column(name = "gender")
+	private String gender;
+	
+	@Column(name = "birthdate")
+	private LocalDate birthdate;
+	
+	@NotNull
+	@Column(name = "birthdate_estimated")
+	private boolean birthdateEstimated;
+	
+	@NotNull
+	@Column(name = "dead")
+	private boolean dead;
+	
+	@Column(name = "death_date")
+	private LocalDate deathDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "cause_of_death")
+	private ConceptLight causeOfDeath;
+	
+	@NotNull
+	@Column(name = "deathdate_estimated")
+	private boolean deathdateEstimated;
+	
+	@Column(name = "birthtime")
+	private LocalTime birthtime;
+	
+	@Column(name = "cause_of_death_non_coded")
+	private String causeOfDeathNonCoded;
 }
