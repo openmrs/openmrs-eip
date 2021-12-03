f2dec1dbed10652b7ffdf415e4965b0ec07ccb75
wluyima <wyclif@openmrs.org>
2021-12-03 13:30:41 +0300

    EIP-61 - Updated spring config to fix issue where DB sync sender and receiver fail to start

--- commons/src/main/java/org/openmrs/eip/app/config/AppConfig.java
+++ commons/src/main/java/org/openmrs/eip/app/config/AppConfig.java
@@ -1,7 +1,8 @@
 package org.openmrs.eip.app.config;
 
+import org.openmrs.eip.config.CommonConfig;
 import org.openmrs.eip.config.DatasourceConfig;
 import org.springframework.context.annotation.Import;
 
-@Import(DatasourceConfig.class)
+@Import({ CommonConfig.class, DatasourceConfig.class })
 public class AppConfig {}
