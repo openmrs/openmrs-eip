6b0354d65044294a9d1899b7040ec4fe66e1ca8a
wluyima <wyclif@openmrs.org>
2021-12-14 19:14:31 +0300

    EC-103 - Added sender camel route to process sync requests

--- camel-openmrs/src/main/java/org/openmrs/eip/component/SyncContext.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/SyncContext.java
@@ -1,8 +1,13 @@
 package org.openmrs.eip.component;
 
+import org.openmrs.eip.component.entity.BaseEntity;
+import org.openmrs.eip.component.exception.EIPException;
+import org.openmrs.eip.component.repository.SyncEntityRepository;
+import org.openmrs.eip.component.service.TableToSyncEnum;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Component;
 
 /**
@@ -25,6 +30,27 @@ public class SyncContext implements ApplicationContextAware {
 	 */
 	public static <T> T getBean(Class<T> clazz) {
 		return appContext.getBean(clazz);
+	}
+	
+	/**
+	 * Gets the {@link SyncEntityRepository} for the entity class mapped to the specified table name
+	 *
+	 * @param tableName the name of the table
+	 * @return SyncEntityRepository for the entity mapped to the table
+	 */
+	public static SyncEntityRepository getRepositoryBean(String tableName) {
+		Class<? extends BaseEntity> entityClass = TableToSyncEnum.getTableToSyncEnum(tableName).getEntityClass();
+		ResolvableType resType = ResolvableType.forClassWithGenerics(SyncEntityRepository.class, entityClass);
+		String[] beanNames = appContext.getBeanNamesForType(resType);
+		if (beanNames.length != 1) {
+			if (beanNames.length == 0) {
+				throw new EIPException("No entity repository found for type: " + entityClass);
+			} else {
+				throw new EIPException("Found multiple entity repositories for type: " + entityClass);
+			}
+		}
+		
+		return (SyncEntityRepository) appContext.getBean(beanNames[0]);
 	}
 	
 }
