18b531239962b78bd5e2ac1be11f3fd322cf4664
wluyima <wyclif@openmrs.org>
2022-02-28 13:42:34 +0300

    Updated BaseWatcherRouteTest.createEvent to delegate to WatcherTestUtils.createEvent

--- openmrs-watcher/src/test/java/org/openmrs/eip/mysql/watcher/route/BaseWatcherRouteTest.java
+++ openmrs-watcher/src/test/java/org/openmrs/eip/mysql/watcher/route/BaseWatcherRouteTest.java
@@ -5,6 +5,7 @@ import static org.openmrs.eip.mysql.watcher.WatcherConstants.PROP_URI_ERROR_HAND
 import org.openmrs.eip.BaseDbBackedCamelTest;
 import org.openmrs.eip.TestConstants;
 import org.openmrs.eip.mysql.watcher.Event;
+import org.openmrs.eip.mysql.watcher.WatcherTestUtils;
 import org.openmrs.eip.mysql.watcher.config.WatcherConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
@@ -26,13 +27,7 @@ public abstract class BaseWatcherRouteTest extends BaseDbBackedCamelTest {
 	protected ConfigurableEnvironment env;
 	
 	protected Event createEvent(String table, String pkId, String identifier, String operation) {
-		Event event = new Event();
-		event.setTableName(table);
-		event.setPrimaryKeyId(pkId);
-		event.setIdentifier(identifier);
-		event.setOperation(operation);
-		event.setSnapshot(false);
-		return event;
+		return WatcherTestUtils.createEvent(table, pkId, identifier, operation);
 	}
 	
 }
