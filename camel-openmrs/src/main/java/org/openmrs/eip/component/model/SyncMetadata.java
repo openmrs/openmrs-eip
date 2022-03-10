970b2d65495ce98e1c57c918dcba51769714dffe
lccpinto <luis.pinto.it@gmail.com>
2022-03-10 15:27:29 +0200

    EC-88 Sender should record sync message sent to activeMQ

--- camel-openmrs/src/main/java/org/openmrs/eip/component/model/SyncMetadata.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/model/SyncMetadata.java
@@ -3,95 +3,105 @@ package org.openmrs.eip.component.model;
 import java.time.LocalDateTime;
 
 /**
- * Encapsulates descriptive data about a sync payload i.e. the unique ID of the site sending the.
- * payload and the date it was sent.
+ * Encapsulates descriptive data about a sync payload i.e. the unique ID of the
+ * site sending the. payload and the date it was sent.
  */
 public class SyncMetadata {
-	
-	private String sourceIdentifier;
-	
-	private String operation;
-	
-	private LocalDateTime dateSent;
-	
-	private String requestUuid;
-	
-	/**
-	 * Gets the sourceIdentifier
-	 *
-	 * @return the sourceIdentifier
-	 */
-	public String getSourceIdentifier() {
-		return sourceIdentifier;
-	}
-	
-	/**
-	 * Sets the sourceIdentifier
-	 *
-	 * @param sourceIdentifier the sourceIdentifier to set
-	 */
-	public void setSourceIdentifier(String sourceIdentifier) {
-		this.sourceIdentifier = sourceIdentifier;
-	}
-	
-	/**
-	 * Gets the operation
-	 *
-	 * @return the operation
-	 */
-	public String getOperation() {
-		return operation;
-	}
-	
-	/**
-	 * Sets the operation
-	 *
-	 * @param operation the operation to set
-	 */
-	public void setOperation(String operation) {
-		this.operation = operation;
-	}
-	
-	/**
-	 * Gets the dateSent
-	 *
-	 * @return the dateSent
-	 */
-	public LocalDateTime getDateSent() {
-		return dateSent;
-	}
-	
-	/**
-	 * Sets the dateSent
-	 *
-	 * @param dateSent the dateSent to set
-	 */
-	public void setDateSent(LocalDateTime dateSent) {
-		this.dateSent = dateSent;
-	}
-	
-	/**
-	 * Gets the requestUuid
-	 *
-	 * @return the requestUuid
-	 */
-	public String getRequestUuid() {
-		return requestUuid;
-	}
-	
-	/**
-	 * Sets the requestUuid
-	 *
-	 * @param requestUuid the requestUuid to set
-	 */
-	public void setRequestUuid(String requestUuid) {
-		this.requestUuid = requestUuid;
-	}
-	
-	@Override
-	public String toString() {
-		return getClass().getSimpleName() + "{sourceIdentifier=" + sourceIdentifier + ", operation=" + operation
-		        + ", dateSent=" + dateSent + ", requestUuid=" + requestUuid + "}";
-	}
-	
+
+    private String sourceIdentifier;
+
+    private String operation;
+
+    private LocalDateTime dateSent;
+
+    private String requestUuid;
+
+    private String messageUuid;
+
+    /**
+     * Gets the sourceIdentifier
+     *
+     * @return the sourceIdentifier
+     */
+    public String getSourceIdentifier() {
+        return sourceIdentifier;
+    }
+
+    /**
+     * Sets the sourceIdentifier
+     *
+     * @param sourceIdentifier the sourceIdentifier to set
+     */
+    public void setSourceIdentifier(String sourceIdentifier) {
+        this.sourceIdentifier = sourceIdentifier;
+    }
+
+    /**
+     * Gets the operation
+     *
+     * @return the operation
+     */
+    public String getOperation() {
+        return operation;
+    }
+
+    /**
+     * Sets the operation
+     *
+     * @param operation the operation to set
+     */
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    /**
+     * Gets the dateSent
+     *
+     * @return the dateSent
+     */
+    public LocalDateTime getDateSent() {
+        return dateSent;
+    }
+
+    /**
+     * Sets the dateSent
+     *
+     * @param dateSent the dateSent to set
+     */
+    public void setDateSent(LocalDateTime dateSent) {
+        this.dateSent = dateSent;
+    }
+
+    /**
+     * Gets the requestUuid
+     *
+     * @return the requestUuid
+     */
+    public String getRequestUuid() {
+        return requestUuid;
+    }
+
+    /**
+     * Sets the requestUuid
+     *
+     * @param requestUuid the requestUuid to set
+     */
+    public void setRequestUuid(String requestUuid) {
+        this.requestUuid = requestUuid;
+    }
+
+    public String getMessageUuid() {
+        return messageUuid;
+    }
+
+    public void setMessageUuid(String messageUuid) {
+        this.messageUuid = messageUuid;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{sourceIdentifier=" + sourceIdentifier + ", operation=" + operation
+                + ", dateSent=" + dateSent + ", requestUuid=" + requestUuid + ", messageUuid=" + messageUuid + "}";
+    }
+
 }
