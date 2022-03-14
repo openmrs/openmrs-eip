65f7894ff46a8e1dd057c0aef9e34f4f11ab7254
lccpinto <luis.pinto.it@gmail.com>
2022-03-14 18:19:47 +0200

    EC-88 Sender should record sync message sent to activeMQ - formatting SyncMetadata class after changing tab policy to Tabs only in eclipse, avoiding non-sense line changing info on merge

--- camel-openmrs/src/main/java/org/openmrs/eip/component/model/SyncMetadata.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/model/SyncMetadata.java
@@ -8,100 +8,100 @@ import java.time.LocalDateTime;
  */
 public class SyncMetadata {
 
-    private String sourceIdentifier;
+	private String sourceIdentifier;
 
-    private String operation;
+	private String operation;
 
-    private LocalDateTime dateSent;
+	private LocalDateTime dateSent;
 
-    private String requestUuid;
+	private String requestUuid;
 
-    private String messageUuid;
+	private String messageUuid;
 
-    /**
-     * Gets the sourceIdentifier
-     *
-     * @return the sourceIdentifier
-     */
-    public String getSourceIdentifier() {
-        return sourceIdentifier;
-    }
+	/**
+	 * Gets the sourceIdentifier
+	 *
+	 * @return the sourceIdentifier
+	 */
+	public String getSourceIdentifier() {
+		return sourceIdentifier;
+	}
 
-    /**
-     * Sets the sourceIdentifier
-     *
-     * @param sourceIdentifier the sourceIdentifier to set
-     */
-    public void setSourceIdentifier(String sourceIdentifier) {
-        this.sourceIdentifier = sourceIdentifier;
-    }
+	/**
+	 * Sets the sourceIdentifier
+	 *
+	 * @param sourceIdentifier the sourceIdentifier to set
+	 */
+	public void setSourceIdentifier(String sourceIdentifier) {
+		this.sourceIdentifier = sourceIdentifier;
+	}
 
-    /**
-     * Gets the operation
-     *
-     * @return the operation
-     */
-    public String getOperation() {
-        return operation;
-    }
+	/**
+	 * Gets the operation
+	 *
+	 * @return the operation
+	 */
+	public String getOperation() {
+		return operation;
+	}
 
-    /**
-     * Sets the operation
-     *
-     * @param operation the operation to set
-     */
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
+	/**
+	 * Sets the operation
+	 *
+	 * @param operation the operation to set
+	 */
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
 
-    /**
-     * Gets the dateSent
-     *
-     * @return the dateSent
-     */
-    public LocalDateTime getDateSent() {
-        return dateSent;
-    }
+	/**
+	 * Gets the dateSent
+	 *
+	 * @return the dateSent
+	 */
+	public LocalDateTime getDateSent() {
+		return dateSent;
+	}
 
-    /**
-     * Sets the dateSent
-     *
-     * @param dateSent the dateSent to set
-     */
-    public void setDateSent(LocalDateTime dateSent) {
-        this.dateSent = dateSent;
-    }
+	/**
+	 * Sets the dateSent
+	 *
+	 * @param dateSent the dateSent to set
+	 */
+	public void setDateSent(LocalDateTime dateSent) {
+		this.dateSent = dateSent;
+	}
 
-    /**
-     * Gets the requestUuid
-     *
-     * @return the requestUuid
-     */
-    public String getRequestUuid() {
-        return requestUuid;
-    }
+	/**
+	 * Gets the requestUuid
+	 *
+	 * @return the requestUuid
+	 */
+	public String getRequestUuid() {
+		return requestUuid;
+	}
 
-    /**
-     * Sets the requestUuid
-     *
-     * @param requestUuid the requestUuid to set
-     */
-    public void setRequestUuid(String requestUuid) {
-        this.requestUuid = requestUuid;
-    }
+	/**
+	 * Sets the requestUuid
+	 *
+	 * @param requestUuid the requestUuid to set
+	 */
+	public void setRequestUuid(String requestUuid) {
+		this.requestUuid = requestUuid;
+	}
 
-    public String getMessageUuid() {
-        return messageUuid;
-    }
+	public String getMessageUuid() {
+		return messageUuid;
+	}
 
-    public void setMessageUuid(String messageUuid) {
-        this.messageUuid = messageUuid;
-    }
+	public void setMessageUuid(String messageUuid) {
+		this.messageUuid = messageUuid;
+	}
 
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "{sourceIdentifier=" + sourceIdentifier + ", operation=" + operation
-                + ", dateSent=" + dateSent + ", requestUuid=" + requestUuid + ", messageUuid=" + messageUuid + "}";
-    }
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + "{sourceIdentifier=" + sourceIdentifier + ", operation=" + operation
+				+ ", dateSent=" + dateSent + ", requestUuid=" + requestUuid + ", messageUuid=" + messageUuid + "}";
+	}
 
 }
