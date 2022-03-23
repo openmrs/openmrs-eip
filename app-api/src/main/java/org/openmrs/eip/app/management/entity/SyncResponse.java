6b62a49a08f4158d60d382619ca0b533431223f7
lccpinto <luis.pinto.it@gmail.com>
2022-03-23 16:13:40 +0200

    EC-88 Sender should record sync message sent to activeMQ - PR changes - merging request-consumer and sync-response-consumer routes

---
+++ app-api/src/main/java/org/openmrs/eip/app/management/entity/SyncResponse.java
@@ -0,0 +1,80 @@
+package org.openmrs.eip.app.management.entity;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "sync_response")
+public class SyncResponse extends AbstractEntity {
+	
+	public static final long serialVersionUID = 1;
+	
+	public enum SyncResponseStatus {
+		NEW,
+		PROCESSED
+	}
+	
+	@NotNull
+	@Column(name = "message_uuid", length = 38, nullable = false, unique = true, updatable = false)
+	@Access(AccessType.FIELD)
+	private String messageUuid;
+	
+	@NotNull
+	@Column(name = "date_sent", precision = 3, nullable = false, updatable = false)
+	@Access(AccessType.FIELD)
+	private Date dateSent;
+	
+	@Column(name = "date_processed", precision = 3)
+	@Access(AccessType.FIELD)
+	private Date dateProcessed;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 10)
+	@Access(AccessType.FIELD)
+	private SyncResponseStatus status;
+	
+	public SyncResponse(@NotNull String messageUuid, @NotNull Date dateSent) {
+		super();
+		this.messageUuid = messageUuid;
+		this.dateSent = dateSent;
+		this.status = SyncResponseStatus.NEW;
+		super.setDateCreated(new Date());
+	}
+	
+	public String getMessageUuid() {
+		return messageUuid;
+	}
+	
+	public Date getDateSent() {
+		return dateSent;
+	}
+	
+	public Date getDateProcessed() {
+		return dateProcessed;
+	}
+	
+	public SyncResponseStatus getStatus() {
+		return status;
+	}
+	
+	public void markAsProcessed() {
+		this.status = SyncResponseStatus.PROCESSED;
+		this.dateProcessed = new Date();
+	}
+	
+	@Override
+	public String toString() {
+		return "SyncResponse [messageUuid=" + messageUuid + ", dateSent=" + dateSent + ", dateProcessed=" + dateProcessed
+		        + ", status=" + status + "]";
+	}
+	
+}
