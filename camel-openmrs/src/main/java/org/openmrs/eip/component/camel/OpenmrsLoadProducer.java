1000e7df586d111ce862bb56e9118d7c379a9d33
wluyima <wyclif@openmrs.org>
2022-04-06 20:07:46 -0500

    EC-143 - Refactored logic that handles save and update events to a convenience method

--- camel-openmrs/src/main/java/org/openmrs/eip/component/camel/OpenmrsLoadProducer.java
+++ camel-openmrs/src/main/java/org/openmrs/eip/component/camel/OpenmrsLoadProducer.java
@@ -76,178 +76,182 @@ public class OpenmrsLoadProducer extends AbstractOpenmrsProducer {
 		if (delete || (isDeleteOperation && (isUser || isProvider) && dbModel == null)) {
 			
 		} else {
-			BaseModel modelToSave = syncModel.getModel();
-			String siteId = syncModel.getMetadata().getSourceIdentifier();
-			if (!isDeleteOperation) {
-				if (isUser) {
-					UserModel userModel = (UserModel) modelToSave;
-					if (dbModel == null) {
-						UserRepository userRepo = SyncContext.getBean(UserRepository.class);
-						if (userModel.getUsername() != null) {
-							User exampleUser = new User();
-							exampleUser.setUsername(userModel.getUsername());
-							Example<User> example = Example.of(exampleUser, matching().withIgnoreCase());
-							if (userRepo.count(example) > 0) {
-								log.info(
-								    "Found a user in the receiver DB with a duplicate username: " + userModel.getUsername()
-								            + ", appending " + "site id to this user's username to make it unique");
-								userModel.setUsername(userModel.getUsername() + VALUE_SITE_SEPARATOR + siteId);
-							}
-						}
-						
+			
+		}
+	}
+	
+	private void processSave(BaseModel dbModel, BaseHashEntity storedHash, Class<? extends BaseHashEntity> hashClass,
+	                         EntityServiceFacade entityServiceFacade, SyncModel syncModel, TableToSyncEnum tableToSyncEnum,
+	                         ProducerTemplate producerTemplate, boolean isUser, boolean isDeleteOperation) {
+		
+		BaseModel modelToSave = syncModel.getModel();
+		String siteId = syncModel.getMetadata().getSourceIdentifier();
+		if (!isDeleteOperation) {
+			if (isUser) {
+				UserModel userModel = (UserModel) modelToSave;
+				if (dbModel == null) {
+					UserRepository userRepo = SyncContext.getBean(UserRepository.class);
+					if (userModel.getUsername() != null) {
 						User exampleUser = new User();
-						exampleUser.setSystemId(userModel.getSystemId());
+						exampleUser.setUsername(userModel.getUsername());
 						Example<User> example = Example.of(exampleUser, matching().withIgnoreCase());
 						if (userRepo.count(example) > 0) {
-							log.info("Found a user in the receiver DB with a duplicate systemId: " + userModel.getSystemId()
-							        + ", appending site id " + "to this user's systemId to make it unique");
-							userModel.setSystemId(userModel.getSystemId() + VALUE_SITE_SEPARATOR + siteId);
+							log.info("Found a user in the receiver DB with a duplicate username: " + userModel.getUsername()
+							        + ", appending " + "site id to this user's username to make it unique");
+							userModel.setUsername(userModel.getUsername() + VALUE_SITE_SEPARATOR + siteId);
 						}
 					}
+					
+					User exampleUser = new User();
+					exampleUser.setSystemId(userModel.getSystemId());
+					Example<User> example = Example.of(exampleUser, matching().withIgnoreCase());
+					if (userRepo.count(example) > 0) {
+						log.info("Found a user in the receiver DB with a duplicate systemId: " + userModel.getSystemId()
+						        + ", appending site id " + "to this user's systemId to make it unique");
+						userModel.setSystemId(userModel.getSystemId() + VALUE_SITE_SEPARATOR + siteId);
+					}
+				}
+			}
+		} else {
+			//This is a user or provider entity that was deleted
+			log.info("Entity was deleted in remote site, marking it as retired");
+			BaseMetadataModel existing = entityServiceFacade.getModel(tableToSyncEnum, syncModel.getModel().getUuid());
+			existing.setRetired(true);
+			existing.setRetiredByUuid(UserLight.class.getName() + "(" + SyncContext.getAppUser().getUuid() + ")");
+			existing.setDateRetired(LocalDateTime.now());
+			existing.setRetireReason(Constants.DEFAULT_RETIRE_REASON);
+			modelToSave = existing;
+		}
+		
+		if (dbModel == null) {
+			if (storedHash == null) {
+				if (log.isDebugEnabled()) {
+					log.debug("Inserting new hash for the incoming entity state");
+				}
+				
+				try {
+					storedHash = HashUtils.instantiateHashEntity(hashClass);
+				}
+				catch (Exception e) {
+					throw new EIPException("Failed to create an instance of " + hashClass, e);
+				}
+				
+				storedHash.setIdentifier(syncModel.getModel().getUuid());
+				storedHash.setDateCreated(LocalDateTime.now());
+				
+				if (log.isDebugEnabled()) {
+					log.debug("Saving hash for the incoming entity state");
 				}
 			} else {
-				//This is a user or provider entity that was deleted
-				log.info("Entity was deleted in remote site, marking it as retired");
-				BaseMetadataModel existing = entityServiceFacade.getModel(tableToSyncEnum, syncModel.getModel().getUuid());
-				existing.setRetired(true);
-				existing.setRetiredByUuid(UserLight.class.getName() + "(" + SyncContext.getAppUser().getUuid() + ")");
-				existing.setDateRetired(LocalDateTime.now());
-				existing.setRetireReason(Constants.DEFAULT_RETIRE_REASON);
-				modelToSave = existing;
+				//This will typically happen if we inserted the hash but something went wrong before or during
+				//insert of the entity and the event comes back as a retry item
+				log.info("Found existing hash for a new entity, this could be a retry item to insert a new entity "
+				        + "where the hash was created but the insert previously failed or a previously deleted entity at another site");
+				storedHash.setDateChanged(LocalDateTime.now());
+				
+				if (log.isDebugEnabled()) {
+					log.debug("Updating hash for the incoming entity state");
+				}
 			}
 			
-			if (dbModel == null) {
-				if (storedHash == null) {
-					if (log.isDebugEnabled()) {
-						log.debug("Inserting new hash for the incoming entity state");
-					}
-					
-					try {
-						storedHash = HashUtils.instantiateHashEntity(hashClass);
-					}
-					catch (Exception e) {
-						throw new EIPException("Failed to create an instance of " + hashClass, e);
-					}
-					
-					storedHash.setIdentifier(syncModel.getModel().getUuid());
-					storedHash.setDateCreated(LocalDateTime.now());
-					
-					if (log.isDebugEnabled()) {
-						log.debug("Saving hash for the incoming entity state");
-					}
-				} else {
-					//This will typically happen if we inserted the hash but something went wrong before or during
-					//insert of the entity and the event comes back as a retry item
-					log.info("Found existing hash for a new entity, this could be a retry item to insert a new entity "
-					        + "where the hash was created but the insert previously failed or a previously deleted entity at another site");
-					storedHash.setDateChanged(LocalDateTime.now());
-					
-					if (log.isDebugEnabled()) {
-						log.debug("Updating hash for the incoming entity state");
+			storedHash.setHash(HashUtils.computeHash(syncModel.getModel()));
+			
+			producerTemplate.sendBody(
+			    Constants.QUERY_SAVE_HASH.replace(Constants.PLACEHOLDER_CLASS, hashClass.getSimpleName()), storedHash);
+			
+			if (log.isDebugEnabled()) {
+				log.debug("Successfully saved the hash for the incoming entity state");
+			}
+			
+			entityServiceFacade.saveModel(tableToSyncEnum, modelToSave);
+		} else {
+			boolean isEtyInDbPlaceHolder = false;
+			if (dbModel instanceof BaseDataModel) {
+				BaseDataModel dataModel = (BaseDataModel) dbModel;
+				isEtyInDbPlaceHolder = dataModel.isVoided() && DEFAULT_VOID_REASON.equals(dataModel.getVoidReason());
+			} else if (dbModel instanceof BaseMetadataModel) {
+				BaseMetadataModel metadataModel = (BaseMetadataModel) dbModel;
+				isEtyInDbPlaceHolder = metadataModel.isRetired()
+				        && DEFAULT_VOID_REASON.equals(metadataModel.getRetireReason());
+			}
+			
+			boolean isNewHashInstance = false;
+			boolean isExistingEntityWithNoHash = false;
+			if (storedHash == null) {
+				isExistingEntityWithNoHash = !isEtyInDbPlaceHolder;
+				if (!isEtyInDbPlaceHolder) {
+					String ignore = SyncContext.getBean(Environment.class).getProperty(Constants.PROP_IGNORE_MISSING_HASH);
+					if (!"true".equals(ignore)) {
+						//TODO Don't fail if hashes of the db and incoming payloads match
+						throw new EIPException("Failed to find the existing hash for an existing entity");
 					}
 				}
-				
-				storedHash.setHash(HashUtils.computeHash(syncModel.getModel()));
-				
-				producerTemplate.sendBody(
-				    Constants.QUERY_SAVE_HASH.replace(Constants.PLACEHOLDER_CLASS, hashClass.getSimpleName()), storedHash);
 				
 				if (log.isDebugEnabled()) {
-					log.debug("Successfully saved the hash for the incoming entity state");
+					if (isEtyInDbPlaceHolder) {
+						log.debug("Inserting new hash for existing placeholder entity");
+					} else {
+						log.debug("Inserting new hash for existing entity with missing hash");
+					}
 				}
 				
-				entityServiceFacade.saveModel(tableToSyncEnum, modelToSave);
+				try {
+					storedHash = HashUtils.instantiateHashEntity(hashClass);
+					isNewHashInstance = true;
+				}
+				catch (Exception e) {
+					throw new EIPException("Failed to create an instance of " + hashClass, e);
+				}
+				
+				storedHash.setIdentifier(syncModel.getModel().getUuid());
+				storedHash.setDateCreated(LocalDateTime.now());
+			}
+			
+			String newHash = HashUtils.computeHash(syncModel.getModel());
+			if (!isExistingEntityWithNoHash && !isEtyInDbPlaceHolder) {
+				String dbEntityHash = HashUtils.computeHash(dbModel);
+				if (!dbEntityHash.equals(storedHash.getHash())) {
+					if (dbEntityHash.equals(newHash)) {
+						//This will typically happen if we update the entity but something goes wrong before or during
+						//update of the hash and the event comes back as a retry item
+						log.info("Stored hash differs from that of the state in the DB, ignoring this because the incoming "
+						        + "and DB states match");
+					} else {
+						throw new ConflictsFoundException();
+					}
+				}
 			} else {
-				boolean isEtyInDbPlaceHolder = false;
-				if (dbModel instanceof BaseDataModel) {
-					BaseDataModel dataModel = (BaseDataModel) dbModel;
-					isEtyInDbPlaceHolder = dataModel.isVoided() && DEFAULT_VOID_REASON.equals(dataModel.getVoidReason());
-				} else if (dbModel instanceof BaseMetadataModel) {
-					BaseMetadataModel metadataModel = (BaseMetadataModel) dbModel;
-					isEtyInDbPlaceHolder = metadataModel.isRetired()
-					        && DEFAULT_VOID_REASON.equals(metadataModel.getRetireReason());
+				if (log.isDebugEnabled()) {
+					if (isEtyInDbPlaceHolder) {
+						log.debug("Ignoring placeholder entity when checking for conflicts");
+					} else if (isExistingEntityWithNoHash) {
+						log.debug("Ignoring existing entity with missing hash when checking for conflicts");
+					}
 				}
-				
-				boolean isNewHashInstance = false;
-				boolean isExistingEntityWithNoHash = false;
-				if (storedHash == null) {
-					isExistingEntityWithNoHash = !isEtyInDbPlaceHolder;
-					if (!isEtyInDbPlaceHolder) {
-						String ignore = SyncContext.getBean(Environment.class)
-						        .getProperty(Constants.PROP_IGNORE_MISSING_HASH);
-						if (!"true".equals(ignore)) {
-							//TODO Don't fail if hashes of the db and incoming payloads match
-							throw new EIPException("Failed to find the existing hash for an existing entity");
-						}
-					}
-					
-					if (log.isDebugEnabled()) {
-						if (isEtyInDbPlaceHolder) {
-							log.debug("Inserting new hash for existing placeholder entity");
-						} else {
-							log.debug("Inserting new hash for existing entity with missing hash");
-						}
-					}
-					
-					try {
-						storedHash = HashUtils.instantiateHashEntity(hashClass);
-						isNewHashInstance = true;
-					}
-					catch (Exception e) {
-						throw new EIPException("Failed to create an instance of " + hashClass, e);
-					}
-					
-					storedHash.setIdentifier(syncModel.getModel().getUuid());
-					storedHash.setDateCreated(LocalDateTime.now());
-				}
-				
-				String newHash = HashUtils.computeHash(syncModel.getModel());
-				if (!isExistingEntityWithNoHash && !isEtyInDbPlaceHolder) {
-					String dbEntityHash = HashUtils.computeHash(dbModel);
-					if (!dbEntityHash.equals(storedHash.getHash())) {
-						if (dbEntityHash.equals(newHash)) {
-							//This will typically happen if we update the entity but something goes wrong before or during
-							//update of the hash and the event comes back as a retry item
-							log.info(
-							    "Stored hash differs from that of the state in the DB, ignoring this because the incoming "
-							            + "and DB states match");
-						} else {
-							throw new ConflictsFoundException();
-						}
-					}
+			}
+			
+			entityServiceFacade.saveModel(tableToSyncEnum, modelToSave);
+			
+			storedHash.setHash(newHash);
+			if (!isNewHashInstance) {
+				storedHash.setDateChanged(LocalDateTime.now());
+			}
+			
+			if (log.isDebugEnabled()) {
+				if (isNewHashInstance) {
+					log.debug("Saving new hash for the entity");
 				} else {
-					if (log.isDebugEnabled()) {
-						if (isEtyInDbPlaceHolder) {
-							log.debug("Ignoring placeholder entity when checking for conflicts");
-						} else if (isExistingEntityWithNoHash) {
-							log.debug("Ignoring existing entity with missing hash when checking for conflicts");
-						}
-					}
+					log.debug("Updating hash for the incoming entity state");
 				}
-				
-				entityServiceFacade.saveModel(tableToSyncEnum, modelToSave);
-				
-				storedHash.setHash(newHash);
-				if (!isNewHashInstance) {
-					storedHash.setDateChanged(LocalDateTime.now());
-				}
-				
-				if (log.isDebugEnabled()) {
-					if (isNewHashInstance) {
-						log.debug("Saving new hash for the entity");
-					} else {
-						log.debug("Updating hash for the incoming entity state");
-					}
-				}
-				
-				producerTemplate.sendBody(QUERY_SAVE_HASH.replace(PLACEHOLDER_CLASS, hashClass.getSimpleName()), storedHash);
-				
-				if (log.isDebugEnabled()) {
-					if (isNewHashInstance) {
-						log.debug("Successfully saved new hash for the entity");
-					} else {
-						log.debug("Successfully updated the hash for the incoming entity state");
-					}
+			}
+			
+			producerTemplate.sendBody(QUERY_SAVE_HASH.replace(PLACEHOLDER_CLASS, hashClass.getSimpleName()), storedHash);
+			
+			if (log.isDebugEnabled()) {
+				if (isNewHashInstance) {
+					log.debug("Successfully saved new hash for the entity");
+				} else {
+					log.debug("Successfully updated the hash for the incoming entity state");
 				}
 			}
 		}
