0bc49a5b48f03efbd62c369d103b96381a4f7aa6
wluyima <wyclif@openmrs.org>
2022-07-27 17:18:18 -0500

    EC-215 - Fixed tests for sender request and response processor routes

--- app-api/src/test/java/org/openmrs/eip/app/route/sender/SenderRequestProcessorRouteTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/route/sender/SenderRequestProcessorRouteTest.java
@@ -13,10 +13,14 @@ import static org.openmrs.eip.app.route.sender.SenderTestUtils.getEntity;
 import static org.openmrs.eip.app.sender.SenderConstants.ROUTE_ID_REQUEST_PROCESSOR;
 import static org.openmrs.eip.app.sender.SenderConstants.URI_REQUEST_PROCESSOR;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.support.DefaultExchange;
 import org.junit.Test;
 import org.openmrs.eip.app.management.entity.DebeziumEvent;
@@ -30,6 +34,13 @@ import ch.qos.logback.classic.Level;
 @TestPropertySource(properties = "logging.level." + ROUTE_ID_REQUEST_PROCESSOR + "=DEBUG")
 public class SenderRequestProcessorRouteTest extends BaseSenderRouteTest {
 	
+	private static final String TEST_LISTENER = "mock:listener";
+	
+	private static final String EX_PROP_SYNC_REQ = "syncRequest";
+	
+	@EndpointInject(TEST_LISTENER)
+	private MockEndpoint mockListener;
+	
 	@Override
 	public String getTestRouteFilename() {
 		return ROUTE_ID_REQUEST_PROCESSOR;
@@ -37,10 +48,10 @@ public class SenderRequestProcessorRouteTest extends BaseSenderRouteTest {
 	
 	private SenderSyncRequest createSyncRequest(String table, String identifier, String requestUuid) {
 		SenderSyncRequest request = new SenderSyncRequest();
-        request.setTableName(table);
-        request.setIdentifier(identifier);
-        request.setRequestUuid(requestUuid);
-        request.setDateCreated(new Date());
+		request.setTableName(table);
+		request.setIdentifier(identifier);
+		request.setRequestUuid(requestUuid);
+		request.setDateCreated(new Date());
 		SenderTestUtils.saveEntity(request);
 		return request;
 	}
@@ -54,22 +65,35 @@ public class SenderRequestProcessorRouteTest extends BaseSenderRouteTest {
 	
 	@Test
 	@Sql(scripts = "classpath:mgt_sender_sync_request.sql", config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
-	public void shouldLoadAndProcessAllNewSyncRequestsSortedByDateCreated() {
+	public void shouldLoadAndProcessAllNewSyncRequestsSortedByDateCreated() throws Exception {
 		final int messageCount = 3;
 		List<SenderSyncRequest> requests = getEntities(SenderSyncRequest.class).stream().filter(m -> m.getStatus() == NEW)
 		        .collect(Collectors.toList());
 		assertEquals(messageCount, requests.size());
 		assertTrue(requests.get(0).getDateCreated().getTime() > (requests.get(2).getDateCreated().getTime()));
 		assertTrue(requests.get(1).getDateCreated().getTime() > (requests.get(2).getDateCreated().getTime()));
+		advise(ROUTE_ID_REQUEST_PROCESSOR, new AdviceWithRouteBuilder() {
+			
+			@Override
+			public void configure() {
+				weaveByToUri("jpa:SenderSyncRequest").after().to(TEST_LISTENER);
+			}
+			
+		});
+		
+		List<SenderSyncRequest> processedRequests = new ArrayList();
+		mockListener.whenAnyExchangeReceived(e -> {
+			processedRequests.add(e.getProperty(EX_PROP_SYNC_REQ, SenderSyncRequest.class));
+		});
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		
 		producerTemplate.send(URI_REQUEST_PROCESSOR, exchange);
 		
 		assertMessageLogged(Level.INFO, "Fetched " + requests.size() + " sync request(s)");
-		assertEquals(messageCount, exchange.getIn().getBody(List.class).size());
-		assertEquals(3, ((SenderSyncRequest) exchange.getIn().getBody(List.class).get(0)).getId().intValue());
-		assertEquals(1, ((SenderSyncRequest) exchange.getIn().getBody(List.class).get(1)).getId().intValue());
-		assertEquals(2, ((SenderSyncRequest) exchange.getIn().getBody(List.class).get(2)).getId().intValue());
+		assertEquals(messageCount, processedRequests.size());
+		assertEquals(3, processedRequests.get(0).getId().intValue());
+		assertEquals(1, processedRequests.get(1).getId().intValue());
+		assertEquals(2, processedRequests.get(2).getId().intValue());
 		assertEquals(0, getEntities(SenderSyncRequest.class).stream().filter(m -> m.getStatus() == NEW).count());
 	}
 	
--- app-api/src/test/java/org/openmrs/eip/app/route/sender/SenderResponseProcessorRouteTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/route/sender/SenderResponseProcessorRouteTest.java
@@ -11,9 +11,14 @@ import static org.openmrs.eip.app.sender.SenderConstants.ROUTE_ID_RESPONSE_PROCE
 import static org.openmrs.eip.app.sender.SenderConstants.URI_RESPONSE_PROCESSOR;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ToDynamicDefinition;
 import org.apache.camel.support.DefaultExchange;
 import org.junit.Test;
 import org.openmrs.eip.app.management.entity.SenderSyncMessage;
@@ -26,6 +31,13 @@ import ch.qos.logback.classic.Level;
 
 @TestPropertySource(properties = "logging.level." + ROUTE_ID_RESPONSE_PROCESSOR + "=DEBUG")
 public class SenderResponseProcessorRouteTest extends BaseSenderRouteTest {
+	
+	private static final String TEST_LISTENER = "mock:listener";
+	
+	private static final String EX_PROP_SYNC_RESP = "syncResponse";
+	
+	@EndpointInject(TEST_LISTENER)
+	private MockEndpoint mockListener;
 	
 	@Override
 	public String getTestRouteFilename() {
@@ -45,29 +57,42 @@ public class SenderResponseProcessorRouteTest extends BaseSenderRouteTest {
 	public void shouldDoNothingIfNoSyncResponsesAreFound() {
 		producerTemplate.send(URI_RESPONSE_PROCESSOR, new DefaultExchange(camelContext));
 		
-		assertMessageLogged(Level.DEBUG, "No sender sync responses was found");
+		assertMessageLogged(Level.DEBUG, "No sync responses was found");
 	}
 	
 	@Test
 	@Sql(scripts = "classpath:mgt_sender_sync_message.sql", config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
 	@Sql(scripts = "classpath:mgt_sender_sync_response.sql", config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
-	public void shouldLoadAndProcessAllNewSyncResponsesSortedByDateCreated() {
+	public void shouldLoadAndProcessAllNewSyncResponsesSortedByDateCreated() throws Exception {
 		final int messageCount = 4;
 		assertFalse(getEntities(SenderSyncMessage.class).isEmpty());
 		List<SenderSyncResponse> responses = getEntities(SenderSyncResponse.class);
 		assertEquals(messageCount, responses.size());
 		assertTrue(responses.get(0).getDateCreated().getTime() > (responses.get(2).getDateCreated().getTime()));
 		assertTrue(responses.get(1).getDateCreated().getTime() > (responses.get(2).getDateCreated().getTime()));
+		advise(ROUTE_ID_RESPONSE_PROCESSOR, new AdviceWithRouteBuilder() {
+			
+			@Override
+			public void configure() {
+				weaveByType(ToDynamicDefinition.class).selectLast().after().to(TEST_LISTENER);
+			}
+			
+		});
+		
+		List<SenderSyncResponse> processedResponses = new ArrayList();
+		mockListener.whenAnyExchangeReceived(e -> {
+			processedResponses.add(e.getProperty(EX_PROP_SYNC_RESP, SenderSyncResponse.class));
+		});
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		
 		producerTemplate.send(URI_RESPONSE_PROCESSOR, exchange);
 		
 		assertMessageLogged(Level.INFO, "Fetched " + responses.size() + " sender sync response(s)");
-		assertEquals(messageCount, exchange.getIn().getBody(List.class).size());
-		assertEquals(3, ((SenderSyncResponse) exchange.getIn().getBody(List.class).get(0)).getId().intValue());
-		assertEquals(4, ((SenderSyncResponse) exchange.getIn().getBody(List.class).get(1)).getId().intValue());
-		assertEquals(1, ((SenderSyncResponse) exchange.getIn().getBody(List.class).get(2)).getId().intValue());
-		assertEquals(2, ((SenderSyncResponse) exchange.getIn().getBody(List.class).get(3)).getId().intValue());
+		assertEquals(messageCount, processedResponses.size());
+		assertEquals(3, processedResponses.get(0).getId().intValue());
+		assertEquals(4, processedResponses.get(1).getId().intValue());
+		assertEquals(1, processedResponses.get(2).getId().intValue());
+		assertEquals(2, processedResponses.get(3).getId().intValue());
 		assertTrue(getEntities(SenderSyncResponse.class).isEmpty());
 		assertTrue(getEntities(SenderSyncMessage.class).isEmpty());
 	}
