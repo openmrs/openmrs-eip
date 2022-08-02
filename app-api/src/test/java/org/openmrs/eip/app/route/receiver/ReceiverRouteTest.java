9713b3235a9f950a98fa829f3e11277acd3ca652
wluyima <wyclif@openmrs.org>
2022-08-02 15:36:04 -0500

    EC-201 - Add remaining tests for receiver-route.xml file

--- app-api/src/test/java/org/openmrs/eip/app/route/receiver/ReceiverRouteTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/route/receiver/ReceiverRouteTest.java
@@ -9,6 +9,7 @@ import static org.openmrs.eip.app.SyncConstants.MGT_TX_MGR;
 import static org.openmrs.eip.app.receiver.ReceiverConstants.EX_PROP_IS_FILE;
 import static org.openmrs.eip.app.receiver.ReceiverConstants.EX_PROP_METADATA;
 import static org.openmrs.eip.app.receiver.ReceiverConstants.PROP_ACTIVEMQ_IN_ENDPOINT;
+import static org.openmrs.eip.app.receiver.ReceiverConstants.PROP_CAMEL_OUTPUT_ENDPOINT;
 import static org.openmrs.eip.app.receiver.ReceiverConstants.ROUTE_ID_COMPLEX_OBS_SYNC;
 import static org.openmrs.eip.app.receiver.ReceiverConstants.ROUTE_ID_RECEIVER_MAIN;
 import static org.openmrs.eip.app.receiver.ReceiverConstants.ROUTE_ID_UPDATE_LAST_SYNC_DATE;
@@ -17,6 +18,7 @@ import static org.openmrs.eip.app.receiver.ReceiverConstants.URI_UPDATE_LAST_SYN
 import static org.openmrs.eip.app.route.TestUtils.getEntities;
 import static org.openmrs.eip.app.route.TestUtils.getEntity;
 import static org.openmrs.eip.app.route.receiver.ReceiverRouteTest.URI_ACTIVEMQ;
+import static org.openmrs.eip.app.route.receiver.ReceiverRouteTest.URI_ACTIVEMQ_RESPONSE_PREFIX;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -29,6 +31,8 @@ import org.apache.camel.support.DefaultExchange;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.eip.app.CustomMessageListenerContainer;
+import org.openmrs.eip.app.management.entity.ReceiverSyncRequest;
+import org.openmrs.eip.app.management.entity.ReceiverSyncRequest.ReceiverRequestStatus;
 import org.openmrs.eip.app.management.entity.SiteInfo;
 import org.openmrs.eip.app.management.entity.SyncMessage;
 import org.openmrs.eip.app.route.TestUtils;
@@ -43,11 +47,15 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 
 @TestPropertySource(properties = PROP_ACTIVEMQ_IN_ENDPOINT + "=" + URI_ACTIVEMQ)
+@TestPropertySource(properties = PROP_CAMEL_OUTPUT_ENDPOINT + "=" + URI_ACTIVEMQ_RESPONSE_PREFIX + "{0}")
 @TestPropertySource(properties = "logging.level." + ROUTE_ID_RECEIVER_MAIN + "=DEBUG")
-@Sql(scripts = "classpath:mgt_site_info.sql", config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
+@Sql(scripts = { "classpath:mgt_site_info.sql",
+        "classpath:mgt_receiver_sync_request.sql" }, config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
 public class ReceiverRouteTest extends BaseReceiverRouteTest {
 	
 	protected static final String URI_ACTIVEMQ = "direct:" + ROUTE_ID_RECEIVER_MAIN;
+	
+	public static final String URI_ACTIVEMQ_RESPONSE_PREFIX = "mock:response.";
 	
 	@EndpointInject("mock:" + ROUTE_ID_UPDATE_LAST_SYNC_DATE)
 	private MockEndpoint mockUpdateSyncStatusEndpoint;
@@ -55,11 +63,15 @@ public class ReceiverRouteTest extends BaseReceiverRouteTest {
 	@EndpointInject("mock:" + ROUTE_ID_COMPLEX_OBS_SYNC)
 	private MockEndpoint mockComplexObsSyncEndpoint;
 	
+	@EndpointInject(URI_ACTIVEMQ_RESPONSE_PREFIX + "remote1")
+	private MockEndpoint mockActiveMqResponseEndpoint;
+	
 	@Before
 	public void setup() throws Exception {
 		Whitebox.setInternalState(CustomMessageListenerContainer.class, "commit", false);
 		mockUpdateSyncStatusEndpoint.reset();
 		mockComplexObsSyncEndpoint.reset();
+		mockActiveMqResponseEndpoint.reset();
 		advise(ROUTE_ID_RECEIVER_MAIN, new AdviceWithRouteBuilder() {
 			
 			@Override
@@ -140,6 +152,77 @@ public class ReceiverRouteTest extends BaseReceiverRouteTest {
 		mockUpdateSyncStatusEndpoint.assertIsSatisfied();
 		mockComplexObsSyncEndpoint.assertIsSatisfied();
 		assertTrue(TestUtils.getEntities(SyncMessage.class).isEmpty());
+		assertTrue(Whitebox.getInternalState(CustomMessageListenerContainer.class, "commit"));
+	}
+	
+	@Test
+	public void shouldProcessAndSaveASyncMessageLinkedToASyncRequest() throws Exception {
+		final LocalDateTime dateSent = LocalDateTime.now();
+		final String uuid = "person-uuid";
+		final String msgUuid = "msg-uuid";
+		final String op = "r";
+		final String requestUuid = "46beb8bd-287c-47f2-9786-a7b98c933c04";
+		ReceiverSyncRequest request = getEntity(ReceiverSyncRequest.class, 4L);
+		assertEquals(requestUuid, request.getRequestUuid());
+		assertEquals(ReceiverRequestStatus.SENT, request.getStatus());
+		assertTrue(TestUtils.getEntities(SyncMessage.class).isEmpty());
+		SyncModel syncModel = new SyncModel();
+		syncModel.setTableToSyncModelClass(PersonModel.class);
+		PersonModel personModel = new PersonModel();
+		personModel.setUuid(uuid);
+		syncModel.setModel(personModel);
+		SyncMetadata metadata = new SyncMetadata();
+		metadata.setMessageUuid(msgUuid);
+		metadata.setSnapshot(false);
+		metadata.setOperation(op);
+		metadata.setRequestUuid(requestUuid);
+		SiteInfo siteInfo = getEntity(SiteInfo.class, 1L);
+		metadata.setSourceIdentifier(siteInfo.getIdentifier());
+		metadata.setDateSent(dateSent);
+		syncModel.setMetadata(metadata);
+		Exchange exchange = new DefaultExchange(camelContext);
+		exchange.getIn().setBody(JsonUtils.marshall(syncModel));
+		mockUpdateSyncStatusEndpoint.expectedMessageCount(1);
+		
+		producerTemplate.send(URI_ACTIVEMQ, exchange);
+		
+		mockUpdateSyncStatusEndpoint.assertIsSatisfied();
+		assertEquals(1, getEntities(SyncMessage.class).size());
+		assertEquals(ReceiverRequestStatus.RECEIVED, getEntity(ReceiverSyncRequest.class, request.getId()).getStatus());
+		assertTrue(Whitebox.getInternalState(CustomMessageListenerContainer.class, "commit"));
+	}
+	
+	@Test
+	public void shouldProcessAndSaveASyncMessageLinkedToASyncRequestAndTheEntityWasNotFound() throws Exception {
+		final LocalDateTime dateSent = LocalDateTime.now();
+		final String msgUuid = "msg-uuid";
+		final String op = "r";
+		final String requestUuid = "46beb8bd-287c-47f2-9786-a7b98c933c04";
+		ReceiverSyncRequest request = getEntity(ReceiverSyncRequest.class, 4L);
+		assertEquals(requestUuid, request.getRequestUuid());
+		assertEquals(ReceiverRequestStatus.SENT, request.getStatus());
+		assertTrue(TestUtils.getEntities(SyncMessage.class).isEmpty());
+		SyncModel syncModel = new SyncModel();
+		SyncMetadata metadata = new SyncMetadata();
+		metadata.setMessageUuid(msgUuid);
+		metadata.setSnapshot(false);
+		metadata.setOperation(op);
+		metadata.setRequestUuid(requestUuid);
+		SiteInfo siteInfo = getEntity(SiteInfo.class, 1L);
+		metadata.setSourceIdentifier(siteInfo.getIdentifier());
+		metadata.setDateSent(dateSent);
+		syncModel.setMetadata(metadata);
+		Exchange exchange = new DefaultExchange(camelContext);
+		exchange.getIn().setBody(JsonUtils.marshall(syncModel));
+		mockUpdateSyncStatusEndpoint.expectedMessageCount(1);
+		mockActiveMqResponseEndpoint.expectedMessageCount(1);
+		
+		producerTemplate.send(URI_ACTIVEMQ, exchange);
+		
+		mockUpdateSyncStatusEndpoint.assertIsSatisfied();
+		mockActiveMqResponseEndpoint.assertIsSatisfied();
+		assertTrue(TestUtils.getEntities(SyncMessage.class).isEmpty());
+		assertEquals(ReceiverRequestStatus.RECEIVED, getEntity(ReceiverSyncRequest.class, request.getId()).getStatus());
 		assertTrue(Whitebox.getInternalState(CustomMessageListenerContainer.class, "commit"));
 	}
 	
