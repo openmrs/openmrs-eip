d7895e3598c4a59a8dccbe2ea4d0c043cf3b45b1
wluyima <wyclif@openmrs.org>
2022-08-12 15:10:27 -0500

    EC-216 - Add tests for ReceiverActiveMqMessagePublisher

---
+++ app-api/src/test/java/org/openmrs/eip/app/utils/ReceiverActiveMqMessagePublisherTest.java
@@ -0,0 +1,81 @@
+package org.openmrs.eip.app.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.openmrs.eip.app.receiver.ReceiverConstants.PROP_CAMEL_OUTPUT_ENDPOINT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.eip.app.BaseReceiverTest;
+import org.openmrs.eip.app.management.entity.ReceiverSyncRequest;
+import org.openmrs.eip.app.management.entity.SiteInfo;
+import org.openmrs.eip.app.management.entity.SyncMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestPropertySource;
+
+import com.jayway.jsonpath.JsonPath;
+
+@TestPropertySource(properties = PROP_CAMEL_OUTPUT_ENDPOINT + "="
+        + ReceiverActiveMqMessagePublisherTest.URI_ACTIVEMQ_RESPONSE_PREFIX + "{0}")
+public class ReceiverActiveMqMessagePublisherTest extends BaseReceiverTest {
+	
+	public static final String URI_ACTIVEMQ_RESPONSE_PREFIX = "mock:response.";
+	
+	public static final String SENDER_ID = "test-sender-id";
+	
+	@Autowired
+	ReceiverActiveMqMessagePublisher publisher;
+	
+	@EndpointInject(URI_ACTIVEMQ_RESPONSE_PREFIX + SENDER_ID)
+	private MockEndpoint mockActiveMqEndpoint;
+	
+	@Before
+	public void setup() {
+		mockActiveMqEndpoint.reset();
+	}
+	
+	@Test
+	public void sendSyncResponse_shouldGenerateAndSendAResponseForASyncMessage() throws Exception {
+		final String msgUuid = "message-uuid";
+		SiteInfo siteInfo = new SiteInfo();
+		siteInfo.setIdentifier(SENDER_ID);
+		SyncMessage message = new SyncMessage();
+		message.setMessageUuid(msgUuid);
+		message.setSite(siteInfo);
+		mockActiveMqEndpoint.expectedMessageCount(1);
+		List<String> responses = new ArrayList();
+		mockActiveMqEndpoint.whenAnyExchangeReceived(e -> responses.add(e.getIn().getBody(String.class)));
+		
+		publisher.sendSyncResponse(message);
+		
+		mockActiveMqEndpoint.assertIsSatisfied();
+		assertEquals(1, responses.size());
+		assertEquals(msgUuid, JsonPath.read(responses.get(0), "messageUuid"));
+		assertNotNull(JsonPath.read(responses.get(0), "dateSentByReceiver"));
+	}
+	
+	@Test
+	public void sendSyncResponse_shouldGenerateAndSendAResponseForASyncRequest() throws Exception {
+		final String msgUuid = "message-uuid";
+		SiteInfo siteInfo = new SiteInfo();
+		siteInfo.setIdentifier(SENDER_ID);
+		ReceiverSyncRequest request = new ReceiverSyncRequest();
+		request.setSite(siteInfo);
+		mockActiveMqEndpoint.expectedMessageCount(1);
+		List<String> responses = new ArrayList();
+		mockActiveMqEndpoint.whenAnyExchangeReceived(e -> responses.add(e.getIn().getBody(String.class)));
+		
+		publisher.sendSyncResponse(request, msgUuid);
+		
+		mockActiveMqEndpoint.assertIsSatisfied();
+		assertEquals(1, responses.size());
+		assertEquals(msgUuid, JsonPath.read(responses.get(0), "messageUuid"));
+		assertNotNull(JsonPath.read(responses.get(0), "dateSentByReceiver"));
+	}
+	
+}
