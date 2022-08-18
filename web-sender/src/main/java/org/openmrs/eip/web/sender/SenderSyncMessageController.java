3824b29d62c109c5cba80f2b8bf401d76264d941
wluyima <wyclif@openmrs.org>
2022-08-18 14:14:52 -0500

    Revert "EC-215 - Updated sync message queue to only display NEW messages"
    This reverts commit ddf2d786

--- web-sender/src/main/java/org/openmrs/eip/web/sender/SenderSyncMessageController.java
+++ web-sender/src/main/java/org/openmrs/eip/web/sender/SenderSyncMessageController.java
@@ -1,13 +1,5 @@
 package org.openmrs.eip.web.sender;
 
-import static org.apache.camel.impl.engine.DefaultFluentProducerTemplate.on;
-import static org.openmrs.eip.web.RestConstants.DEFAULT_MAX_COUNT;
-import static org.openmrs.eip.web.RestConstants.FIELD_COUNT;
-import static org.openmrs.eip.web.RestConstants.FIELD_ITEMS;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.openmrs.eip.app.management.entity.SenderSyncMessage;
@@ -15,6 +7,7 @@ import org.openmrs.eip.web.RestConstants;
 import org.openmrs.eip.web.contoller.BaseRestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,24 +30,7 @@ public class SenderSyncMessageController extends BaseRestController {
 			log.debug("Fetching sender sync messages");
 		}
 		
-		Map<String, Object> results = new HashMap(2);
-		Integer count = on(camelContext)
-		        .to("jpa:" + getName() + "?query=SELECT count(*) FROM " + getName() + " WHERE status = 'NEW'")
-		        .request(Integer.class);
-		
-		results.put(FIELD_COUNT, count);
-		
-		List<Object> items;
-		if (count > 0) {
-			items = on(camelContext).to("jpa:" + getName() + "?query=SELECT m FROM " + getName()
-			        + " m WHERE m.status = 'NEW' &maximumResults=" + DEFAULT_MAX_COUNT).request(List.class);
-			
-			results.put(FIELD_ITEMS, items);
-		} else {
-			results.put(FIELD_ITEMS, Collections.emptyList());
-		}
-		
-		return results;
+		return doGetAll();
 	}
 	
 	@GetMapping("/{id}")
