7bdcdc86752d443352b09954fb47e3edcc6b81aa
wluyima <wyclif@openmrs.org>
2022-08-19 23:13:39 -0400

    EC-179 - Sender dashboard should reload every minute

--- webapp/src/main/webapp/src/app/shared/dashboard.component.ts
+++ webapp/src/main/webapp/src/app/shared/dashboard.component.ts
@@ -6,14 +6,28 @@ import {Dashboard} from "./dashboard";
 export abstract class DashboardComponent implements OnInit {
 
 	dashboard?: Dashboard;
+	interval?: number;
 
 	constructor(private service: DashboardService) {
 	}
 
 	ngOnInit(): void {
+		this.loadDashboard();
+		this.interval = setInterval(() => {
+			this.loadDashboard();
+		}, 60000);
+	}
+
+	loadDashboard(): void {
 		this.service.getDashboard().subscribe(dashboard => {
 			this.dashboard = dashboard;
-		})
+		});
+	}
+
+	ngOnDestroy(): void {
+		if (this.interval) {
+			clearInterval(this.interval);
+		}
 	}
 
 }
