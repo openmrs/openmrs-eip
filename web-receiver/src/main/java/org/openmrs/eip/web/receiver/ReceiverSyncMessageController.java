2add7345caa0e4748f2de3316338d3d5e5c56432
wluyima <wyclif@openmrs.org>
2022-08-22 11:43:06 -0500

    EC-182 - Added controller to fetch sync message queue items

---
+++ web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncMessageController.java
@@ -0,0 +1,44 @@
+package org.openmrs.eip.web.receiver;
+
+import java.util.Map;
+
+import org.openmrs.eip.app.management.entity.SyncMessage;
+import org.openmrs.eip.web.RestConstants;
+import org.openmrs.eip.web.contoller.BaseRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(RestConstants.API_PATH + "/dbsync/receiver/sync")
+public class ReceiverSyncMessageController extends BaseRestController {
+	
+	private static final Logger log = LoggerFactory.getLogger(ReceiverSyncMessageController.class);
+	
+	@Override
+	public Class<?> getClazz() {
+		return SyncMessage.class;
+	}
+	
+	@GetMapping
+	public Map<String, Object> getAll() {
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching receiver sync messages");
+		}
+		
+		return doGetAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Object get(@PathVariable("id") Integer id) {
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching receiver sync messages with id: " + id);
+		}
+		
+		return doGet(id);
+	}
+	
+}
---
+++ web-receiver/src/test/java/org/openmrs/eip/web/receiver/ReceiverSyncMessageControllerTest.java
@@ -0,0 +1,37 @@
+package org.openmrs.eip.web.receiver;
+
+import static org.junit.Assert.assertEquals;
+import static org.openmrs.eip.app.SyncConstants.MGT_DATASOURCE_NAME;
+import static org.openmrs.eip.app.SyncConstants.MGT_TX_MGR;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.openmrs.eip.app.BaseReceiverTest;
+import org.openmrs.eip.app.management.entity.SyncMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+@Sql(scripts = { "classpath:mgt_site_info.sql",
+        "classpath:mgt_receiver_sync_msg.sql" }, config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
+public class ReceiverSyncMessageControllerTest extends BaseReceiverTest {
+	
+	@Autowired
+	private ReceiverSyncMessageController controller;
+	
+	@Test
+	public void shouldGetAllSyncMessages() {
+		Map result = controller.getAll();
+		assertEquals(2, result.size());
+		assertEquals(3, result.get("count"));
+		assertEquals(3, ((List) result.get("items")).size());
+	}
+	
+	@Test
+	public void shouldGetTheSyncMessageMatchingTheSpecifiedId() {
+		assertEquals("27beb8bd-287c-47f2-9786-a7b98c933c05", ((SyncMessage) controller.get(2)).getMessageUuid());
+	}
+	
+}
