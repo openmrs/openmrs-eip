f87b953bc5f8e9d143845f3a4d01518b891f32cd
wluyima <wyclif@openmrs.org>
2022-08-22 11:43:42 -0500

    EC-182 - Added controller to fetch sync message queue items

---
+++ app-api/src/test/resources/mgt_receiver_sync_msg.sql
@@ -0,0 +1,4 @@
+INSERT INTO receiver_sync_msg (id, model_class_name, identifier, message_uuid, site_id, entity_payload, is_snapshot, date_created)
+VALUES (1, 'org.openmrs.eip.component.model.PersonModel', '1bfd940e-32dc-491f-8038-a8f3afe3e36c', '17beb8bd-287c-47f2-9786-a7b98c933c05', 1, '{}', 0, '2020-08-22 00:00:00.001'),
+       (2, 'org.openmrs.eip.component.model.PersonModel', '2bfd940e-32dc-491f-8038-a8f3afe3e36c', '27beb8bd-287c-47f2-9786-a7b98c933c05', 1, '{}', 0, '2020-08-22 00:00:00.002'),
+       (3, 'org.openmrs.eip.component.model.PersonModel', '1bfd940e-32dc-491f-8038-a8f3afe3e36c', '37beb8bd-287c-47f2-9786-a7b98c933c05', 1, '{}', 0, '2020-08-22 01:00:00.000');
