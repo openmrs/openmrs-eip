e1bee4eec25190bdc5bb1d7d4479f57a2b5992da
wluyima <wyclif@openmrs.org>
2022-08-31 15:30:52 -0500

    EC-222 - Added parallel sync response processor for sync response messages

---
+++ app-api/src/main/java/org/openmrs/eip/app/sender/SenderSyncResponseProcessor.java
@@ -0,0 +1,43 @@
+package org.openmrs.eip.app.sender;
+
+import org.openmrs.eip.app.BaseSenderQueueProcessor;
+import org.openmrs.eip.app.management.entity.SenderSyncResponse;
+import org.openmrs.eip.component.SyncProfiles;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("senderSyncResponseProcessor")
+@Profile(SyncProfiles.SENDER)
+public class SenderSyncResponseProcessor extends BaseSenderQueueProcessor<SenderSyncResponse> {
+	
+	@Override
+	public String getProcessorName() {
+		return "sync response";
+	}
+	
+	@Override
+	public String getThreadName(SenderSyncResponse response) {
+		return response.getMessageUuid() + "-" + response.getId();
+	}
+	
+	@Override
+	public String getItemKey(SenderSyncResponse item) {
+		return item.getId().toString();
+	}
+	
+	@Override
+	public boolean isSnapshot(SenderSyncResponse item) {
+		return true;
+	}
+	
+	@Override
+	public String getQueueName() {
+		return "sync-response";
+	}
+	
+	@Override
+	public String getDestinationUri() {
+		return SenderConstants.URI_RESPONSE_PROCESSOR;
+	}
+	
+}
---
+++ app/src/test/java/org/openmrs/eip/app/sender/SenderSyncResponseProcessorTest.java
@@ -0,0 +1,51 @@
+package org.openmrs.eip.app.sender;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.eip.app.management.entity.SenderSyncResponse;
+
+public class SenderSyncResponseProcessorTest {
+	
+	private SenderSyncResponseProcessor processor = new SenderSyncResponseProcessor();
+	
+	@Test
+	public void getProcessorName_shouldReturnTheProcessorName() {
+		assertEquals("sync response", processor.getProcessorName());
+	}
+	
+	@Test
+	public void getThreadName_shouldReturnTheThreadNameContainingEventDetails() {
+		final String messageUuid = "message-uuid";
+		final Long id = 2L;
+		SenderSyncResponse msg = new SenderSyncResponse();
+		msg.setId(id);
+		msg.setMessageUuid(messageUuid);
+		assertEquals(messageUuid + "-" + id, processor.getThreadName(msg));
+	}
+	
+	@Test
+	public void getItemKey_shouldReturnTheKeyContainingTableAndId() {
+		final Long id = 2L;
+		SenderSyncResponse msg = new SenderSyncResponse();
+		msg.setId(id);
+		assertEquals(id.toString(), processor.getItemKey(msg));
+	}
+	
+	@Test
+	public void isSnapshot_returnTrue() {
+		Assert.assertTrue(processor.isSnapshot(null));
+	}
+	
+	@Test
+	public void getQueueName_shouldReturnTheQueueName() {
+		assertEquals("sync-response", processor.getQueueName());
+	}
+	
+	@Test
+	public void getDestinationUri_shouldReturnTheUriToSendToEventsForProcessing() {
+		assertEquals(SenderConstants.URI_RESPONSE_PROCESSOR, processor.getDestinationUri());
+	}
+	
+}
