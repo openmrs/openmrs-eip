ebbf0c3ddf921cf23554463cd93d33cf05eab2ca
wluyima <wyclif@openmrs.org>
2022-09-27 14:23:04 -0500

    EC-234 - Added remaining implementation code for liquibase changeset that sets date sent by sender

--- app-api/src/main/java/org/openmrs/eip/app/management/receiver/SetDateSentBySenderChangeSet.java
+++ app-api/src/main/java/org/openmrs/eip/app/management/receiver/SetDateSentBySenderChangeSet.java
@@ -1,13 +1,19 @@
 package org.openmrs.eip.app.management.receiver;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.openmrs.eip.component.exception.EIPException;
+import org.openmrs.eip.component.model.SyncMetadata;
+import org.openmrs.eip.component.model.SyncModel;
+import org.openmrs.eip.component.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +34,9 @@ public class SetDateSentBySenderChangeSet implements CustomTaskChange {
 	
 	private static final Logger log = LoggerFactory.getLogger(SetDateSentBySenderChangeSet.class);
 	
-	private static final String SQL = "UPDATE receiver_sync_msg SET date_sent_by_sender = ? WHERE id = ?";
+	private static final String QUERY = "SELECT id, entity_payload FROM receiver_sync_msg WHERE date_sent_by_sender IS NULL LIMIT 1000";
+	
+	private static final String UPDATE_SQL = "UPDATE receiver_sync_msg SET date_sent_by_sender = ? WHERE id = ?";
 	
 	@Override
 	public void execute(Database database) throws CustomChangeException {
@@ -36,13 +44,43 @@ public class SetDateSentBySenderChangeSet implements CustomTaskChange {
 		
 		try {
 			JdbcConnection conn = (JdbcConnection) database.getConnection();
-			
-			runBatchUpdate(null, conn);
+			Map<Long, String> idAndPayloadMap = fetchBatchOfMessageDetails(conn);
+			while (idAndPayloadMap.size() > 0) {
+				Map<Long, LocalDateTime> idAndDateMap = new HashMap(idAndPayloadMap.size());
+				idAndPayloadMap.entrySet().parallelStream().forEach(e -> {
+					SyncMetadata metadata = JsonUtils.unmarshal(e.getValue(), SyncModel.class).getMetadata();
+					idAndDateMap.put(e.getKey(), metadata.getDateSent());
+				});
+				
+				runBatchUpdate(idAndDateMap, conn);
+				
+				idAndPayloadMap = fetchBatchOfMessageDetails(conn);
+			}
 		}
 		catch (SQLException | DatabaseException e) {
 			throw new CustomChangeException("An error occurred while setting date_sent_by_sender for existing sync messages",
 			        e);
 		}
+	}
+	
+	/**
+	 * Fetches the next batch of sync message details
+	 * 
+	 * @param connection The database connection
+	 * @return Map of sync message ids and their payloads
+	 * @throws Exception
+	 */
+	protected Map<Long, String> fetchBatchOfMessageDetails(JdbcConnection connection)
+	    throws SQLException, DatabaseException {
+		
+		Map<Long, String> idAndPayloadMap = new HashMap(1000);
+		try (Statement s = connection.createStatement(); ResultSet rs = s.executeQuery(QUERY)) {
+			while (rs.next()) {
+				idAndPayloadMap.put(rs.getLong(1), rs.getString(2));
+			}
+		}
+		
+		return idAndPayloadMap;
 	}
 	
 	/**
@@ -56,7 +94,7 @@ public class SetDateSentBySenderChangeSet implements CustomTaskChange {
 	    throws SQLException, DatabaseException {
 		
 		Boolean autoCommit = null;
-		try (PreparedStatement s = connection.prepareStatement(SQL);) {
+		try (PreparedStatement s = connection.prepareStatement(UPDATE_SQL);) {
 			autoCommit = connection.getAutoCommit();
 			connection.setAutoCommit(false);
 			
@@ -87,7 +125,7 @@ public class SetDateSentBySenderChangeSet implements CustomTaskChange {
 	
 	@Override
 	public String getConfirmationMessage() {
-		return getClass().getSimpleName() + " completed successfully";
+		return "Setting date_sent_by_sender for existing sync messages completed successfully";
 	}
 	
 	@Override
