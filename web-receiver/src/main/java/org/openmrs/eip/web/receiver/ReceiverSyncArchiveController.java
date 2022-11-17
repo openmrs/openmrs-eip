0ce38f4a9e1e3a18fa70337422ce0268259c6859
wluyima <wyclif@openmrs.org>
2022-11-17 11:35:22 +0300

    EC-257 - Deleted BaseReceiverController class

--- web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncArchiveController.java
+++ web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncArchiveController.java
@@ -1,14 +1,20 @@
 package org.openmrs.eip.web.receiver;
 
+import static org.openmrs.eip.web.RestConstants.FIELD_COUNT;
+import static org.openmrs.eip.web.RestConstants.FIELD_ITEMS;
 import static org.openmrs.eip.web.RestConstants.PARAM_END_DATE;
 import static org.openmrs.eip.web.RestConstants.PARAM_GRP_PROP;
 import static org.openmrs.eip.web.RestConstants.PARAM_START_DATE;
 
 import java.text.ParseException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.openmrs.eip.app.management.entity.receiver.ReceiverSyncArchive;
 import org.openmrs.eip.web.RestConstants;
+import org.openmrs.eip.web.contoller.BaseRestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(RestConstants.PATH_RECEIVER_ARCHIVE)
-public class ReceiverSyncArchiveController extends BaseReceiverController {
+public class ReceiverSyncArchiveController extends BaseRestController {
 	
 	private static final Logger log = LoggerFactory.getLogger(ReceiverSyncArchiveController.class);
 	
@@ -49,8 +55,30 @@ public class ReceiverSyncArchiveController extends BaseReceiverController {
 	}
 	
 	@GetMapping(params = PARAM_GRP_PROP)
-	public Object getGroupedSyncMessages(@RequestParam(PARAM_GRP_PROP) String groupProperty) {
-		return getGroupedItems(groupProperty);
+	public Object getGroupedArchives(@RequestParam(PARAM_GRP_PROP) String groupProperty) {
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching receiver sync archives grouped by " + groupProperty);
+		}
+		
+		Map<String, Object> results = new HashMap(2);
+		Integer count = getAllCount();
+		results.put(FIELD_COUNT, count);
+		
+		if (count > 0) {
+			List<Object[]> items = producerTemplate.requestBody("jpa:" + getName() + "?query=SELECT e." + groupProperty
+			        + ", count(*) FROM " + getName() + " e GROUP BY e." + groupProperty,
+			    null, List.class);
+			final Map<String, Integer> propCountMap = new HashMap();
+			items.forEach(values -> {
+				propCountMap.put(values[0].toString(), Integer.valueOf(values[1].toString()));
+			});
+			
+			results.put(FIELD_ITEMS, propCountMap);
+		} else {
+			results.put(FIELD_ITEMS, Collections.emptyMap());
+		}
+		
+		return results;
 	}
 	
 }
--- web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncMessageController.java
+++ web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverSyncMessageController.java
@@ -1,12 +1,18 @@
 package org.openmrs.eip.web.receiver;
 
+import static org.openmrs.eip.web.RestConstants.FIELD_COUNT;
+import static org.openmrs.eip.web.RestConstants.FIELD_ITEMS;
 import static org.openmrs.eip.web.RestConstants.PARAM_GRP_PROP;
 import static org.openmrs.eip.web.RestConstants.PATH_VAR;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.openmrs.eip.app.management.entity.SyncMessage;
 import org.openmrs.eip.web.RestConstants;
+import org.openmrs.eip.web.contoller.BaseRestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(RestConstants.PATH_RECEIVER_SYNC_MSG)
-public class ReceiverSyncMessageController extends BaseReceiverController {
+public class ReceiverSyncMessageController extends BaseRestController {
 	
 	private static final Logger log = LoggerFactory.getLogger(ReceiverSyncMessageController.class);
 	
@@ -46,7 +52,29 @@ public class ReceiverSyncMessageController extends BaseReceiverController {
 	
 	@GetMapping(params = PARAM_GRP_PROP)
 	public Object getGroupedSyncMessages(@RequestParam(PARAM_GRP_PROP) String groupProperty) {
-		return getGroupedItems(groupProperty);
+		if (log.isDebugEnabled()) {
+			log.debug("Fetching receiver sync messages grouped by " + groupProperty);
+		}
+		
+		Map<String, Object> results = new HashMap(2);
+		Integer count = getAllCount();
+		results.put(FIELD_COUNT, count);
+		
+		if (count > 0) {
+			List<Object[]> items = producerTemplate.requestBody("jpa:" + getName() + "?query=SELECT e." + groupProperty
+			        + ", count(*) FROM " + getName() + " e GROUP BY e." + groupProperty,
+			    null, List.class);
+			final Map<String, Integer> propCountMap = new HashMap();
+			items.forEach(values -> {
+				propCountMap.put(values[0].toString(), Integer.valueOf(values[1].toString()));
+			});
+			
+			results.put(FIELD_ITEMS, propCountMap);
+		} else {
+			results.put(FIELD_ITEMS, Collections.emptyMap());
+		}
+		
+		return results;
 	}
 	
 }
