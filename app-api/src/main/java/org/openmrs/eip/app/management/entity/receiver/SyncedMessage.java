744b5c6ebf60577dbd4e5c48ad260e48a6e059c7
wluyima <wyclif@openmrs.org>
2023-02-15 17:12:49 +0300

    EC-301 - Added DB table and entity class for storing processed messages
    EC-301 - Added itemized property and renamed ProcessedMessage to SyncedMessage

---
+++ app-api/src/main/java/org/openmrs/eip/app/management/entity/receiver/SyncedMessage.java
@@ -0,0 +1,261 @@
+package org.openmrs.eip.app.management.entity.receiver;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.openmrs.eip.app.management.entity.AbstractEntity;
+import org.openmrs.eip.app.management.entity.ConflictQueueItem;
+import org.openmrs.eip.app.management.entity.ReceiverRetryQueueItem;
+import org.openmrs.eip.app.management.entity.SiteInfo;
+import org.openmrs.eip.app.management.entity.SyncMessage;
+import org.openmrs.eip.component.SyncOperation;
+import org.springframework.beans.BeanUtils;
+
+@Entity
+@Table(name = "receiver_synced_msg")
+public class SyncedMessage extends AbstractEntity {
+	
+	public static final long serialVersionUID = 1;
+	
+	@Column(nullable = false, updatable = false)
+	private String identifier;
+	
+	@Column(name = "entity_payload", columnDefinition = "text", nullable = false)
+	private String entityPayload;
+	
+	@Column(name = "model_class_name", nullable = false, updatable = false)
+	private String modelClassName;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, updatable = false, length = 1)
+	private SyncOperation operation;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "site_id", nullable = false, updatable = false)
+	private SiteInfo site;
+	
+	@Column(name = "is_snapshot", nullable = false, updatable = false)
+	private Boolean snapshot = false;
+	
+	@Column(name = "message_uuid", length = 38, updatable = false)
+	private String messageUuid;
+	
+	@NotNull
+	@Column(name = "date_sent_by_sender", nullable = false, updatable = false)
+	private LocalDateTime dateSentBySender;
+	
+	@Column(name = "date_received", updatable = false)
+	private Date dateReceived;
+	
+	@NotNull
+	@Column(name = "is_itemized", nullable = false)
+	private boolean itemized;
+	
+	public SyncedMessage() {
+	}
+	
+	public SyncedMessage(SyncMessage syncMessage) {
+		BeanUtils.copyProperties(syncMessage, this, "id", "dateCreated");
+		setDateReceived(syncMessage.getDateCreated());
+		setItemized(false);
+	}
+	
+	public SyncedMessage(ReceiverRetryQueueItem retry) {
+		BeanUtils.copyProperties(retry, this, "id", "dateCreated");
+		setItemized(false);
+	}
+	
+	public SyncedMessage(ConflictQueueItem conflict) {
+		BeanUtils.copyProperties(conflict, this, "id", "dateCreated");
+		setItemized(false);
+	}
+	
+	/**
+	 * Gets the identifier
+	 *
+	 * @return the identifier
+	 */
+	public String getIdentifier() {
+		return identifier;
+	}
+	
+	/**
+	 * Sets the identifier
+	 *
+	 * @param identifier the identifier to set
+	 */
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	
+	/**
+	 * Gets the entityPayload
+	 *
+	 * @return the entityPayload
+	 */
+	public String getEntityPayload() {
+		return entityPayload;
+	}
+	
+	/**
+	 * Sets the entityPayload
+	 *
+	 * @param entityPayload the entityPayload to set
+	 */
+	public void setEntityPayload(String entityPayload) {
+		this.entityPayload = entityPayload;
+	}
+	
+	/**
+	 * Gets the modelClassName
+	 *
+	 * @return the modelClassName
+	 */
+	public String getModelClassName() {
+		return modelClassName;
+	}
+	
+	/**
+	 * Sets the modelClassName
+	 *
+	 * @param modelClassName the modelClassName to set
+	 */
+	public void setModelClassName(String modelClassName) {
+		this.modelClassName = modelClassName;
+	}
+	
+	/**
+	 * Gets the operation
+	 *
+	 * @return the operation
+	 */
+	public SyncOperation getOperation() {
+		return operation;
+	}
+	
+	/**
+	 * Sets the operation
+	 *
+	 * @param operation the operation to set
+	 */
+	public void setOperation(SyncOperation operation) {
+		this.operation = operation;
+	}
+	
+	/**
+	 * Gets the site
+	 *
+	 * @return the site
+	 */
+	public SiteInfo getSite() {
+		return site;
+	}
+	
+	/**
+	 * Sets the site
+	 *
+	 * @param site the site to set
+	 */
+	public void setSite(SiteInfo site) {
+		this.site = site;
+	}
+	
+	/**
+	 * Gets the snapshot
+	 *
+	 * @return the snapshot
+	 */
+	public Boolean getSnapshot() {
+		return snapshot;
+	}
+	
+	/**
+	 * Sets the snapshot
+	 *
+	 * @param snapshot the snapshot to set
+	 */
+	public void setSnapshot(Boolean snapshot) {
+		this.snapshot = snapshot;
+	}
+	
+	public String getMessageUuid() {
+		return messageUuid;
+	}
+	
+	public void setMessageUuid(String messageUuid) {
+		this.messageUuid = messageUuid;
+	}
+	
+	/**
+	 * Gets the dateSentBySender
+	 *
+	 * @return the dateSentBySender
+	 */
+	public LocalDateTime getDateSentBySender() {
+		return dateSentBySender;
+	}
+	
+	/**
+	 * Sets the dateSentBySender
+	 *
+	 * @param dateSentBySender the dateSentBySender to set
+	 */
+	public void setDateSentBySender(LocalDateTime dateSentBySender) {
+		this.dateSentBySender = dateSentBySender;
+	}
+	
+	/**
+	 * Gets the dateReceived
+	 *
+	 * @return the dateReceived
+	 */
+	public Date getDateReceived() {
+		return dateReceived;
+	}
+	
+	/**
+	 * Sets the dateReceived
+	 *
+	 * @param dateReceived the dateReceived to set
+	 */
+	public void setDateReceived(Date dateReceived) {
+		this.dateReceived = dateReceived;
+	}
+	
+	/**
+	 * Gets the itemized
+	 *
+	 * @return the itemized
+	 */
+	public boolean isItemized() {
+		return itemized;
+	}
+	
+	/**
+	 * Sets the itemized
+	 *
+	 * @param itemized the itemized to set
+	 */
+	public void setItemized(boolean itemized) {
+		this.itemized = itemized;
+	}
+	
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " {id=" + getId() + ", identifier=" + identifier + ", modelClassName="
+		        + modelClassName + ", operation=" + operation + ", site=" + site + ", snapshot=" + snapshot
+		        + ", messageUuid=" + messageUuid + ", dateSentBySender=" + dateSentBySender + ", dateReceived="
+		        + dateReceived + ", itemized=" + itemized + "}";
+	}
+	
+}
