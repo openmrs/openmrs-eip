68b504e7709c497c2517149a43e57537a1496811
wluyima <wyclif@openmrs.org>
2023-03-23 13:53:47 +0300

    EC-486 - Refactored code to only process the latest event for the same entity from SearchIndexUpdatingProcessor to BaseSendToCamelPostSyncActionProcessor

--- app-api/src/main/java/org/openmrs/eip/app/receiver/BaseSendToCamelPostSyncActionProcessor.java
+++ app-api/src/main/java/org/openmrs/eip/app/receiver/BaseSendToCamelPostSyncActionProcessor.java
@@ -1,18 +1,29 @@
 package org.openmrs.eip.app.receiver;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.apache.camel.ProducerTemplate;
+import org.apache.commons.collections.CollectionUtils;
+import org.openmrs.eip.app.AppUtils;
 import org.openmrs.eip.app.BaseQueueProcessor;
 import org.openmrs.eip.app.SendToCamelEndpointProcessor;
 import org.openmrs.eip.app.management.entity.receiver.SyncedMessage;
 import org.openmrs.eip.app.management.repository.SyncedMessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Superclass for post sync action processors that send an item to a camel endpoint uri for
  * processing
  */
 public abstract class BaseSendToCamelPostSyncActionProcessor extends BaseQueueProcessor<SyncedMessage> implements SendToCamelEndpointProcessor<SyncedMessage> {
+	
+	protected static final Logger log = LoggerFactory.getLogger(BaseSendToCamelPostSyncActionProcessor.class);
 	
 	private String endpointUri;
 	
@@ -29,6 +40,65 @@ public abstract class BaseSendToCamelPostSyncActionProcessor extends BaseQueuePr
 	}
 	
 	@Override
+	public String getUniqueId(SyncedMessage item) {
+		return item.getId().toString();
+	}
+	
+	@Override
+	public String getLogicalType(SyncedMessage item) {
+		//Since we squash msgs for the same entity so need to worry about parallel msg processing for the same entity
+		return item.getClass().getName();
+	}
+	
+	@Override
+	public List<String> getLogicalTypeHierarchy(String logicalType) {
+		//Since we squash events for the same entity so need to worry about parallel msg processing for the same entity
+		return null;
+	}
+	
+	@Override
+	public String getThreadName(SyncedMessage item) {
+		return item.getSite().getIdentifier() + "-" + item.getMessageUuid() + "-"
+		        + AppUtils.getSimpleName(item.getModelClassName()) + "-" + item.getIdentifier();
+	}
+	
+	@Override
+	public void processWork(List<SyncedMessage> items) throws Exception {
+		//Squash events for the same entity to the latest, this ensures exactly one message for the same entity in case 
+		//of multiple in this run in an effort to reduce calls to OpenMRS endpoints
+		Map<String, SyncedMessage> keyAndLatestMsgMap = new LinkedHashMap(items.size());
+		items.stream().forEach(msg -> {
+			String modelClass = msg.getModelClassName();
+			if (ReceiverUtils.isSubclass(modelClass)) {
+				String parentClass = ReceiverUtils.getParentModelClassName(modelClass);
+				
+				if (log.isTraceEnabled()) {
+					log.trace("Parent model class name for " + modelClass + " is " + parentClass);
+				}
+				
+				modelClass = parentClass;
+			}
+			
+			keyAndLatestMsgMap.put(modelClass + "#" + msg.getIdentifier(), msg);
+		});
+		
+		Collection<SyncedMessage> latest = keyAndLatestMsgMap.values();
+		
+		//Call the endpoint only for the latest events for each entity
+		doProcessWork(new ArrayList(latest));
+		
+		Collection<SyncedMessage> skipped = CollectionUtils.subtract(items, latest);
+		skipped.stream().forEach(msg -> updateSquashedMessage(msg));
+		
+		//Earlier events for each entity get marked as processed in the DB without calling OpenMRS endpoints
+		doProcessWork(new ArrayList(skipped));
+	}
+	
+	protected void doProcessWork(List<SyncedMessage> items) throws Exception {
+		super.processWork(items);
+	}
+	
+	@Override
 	public void processItem(SyncedMessage item) {
 		if (!skipSend(item)) {
 			send(endpointUri, item, producerTemplate);
@@ -38,18 +108,25 @@ public abstract class BaseSendToCamelPostSyncActionProcessor extends BaseQueuePr
 	}
 	
 	/**
+	 * Post-processes the message upon success
+	 *
+	 * @param item the item that was successfully processed
+	 */
+	public abstract void onSuccess(SyncedMessage item);
+	
+	/**
 	 * Checks if the specified item should not be sent to the camel endpoint
-	 * 
+	 *
 	 * @param item the item to check
 	 * @return true if the item should not be sent otherwise false
 	 */
 	public abstract boolean skipSend(SyncedMessage item);
 	
 	/**
-	 * Post-processes the message upon success
+	 * Processes the specified squashed {@link SyncedMessage}
 	 *
-	 * @param item the item that was successfully processed
+	 * @param item the squashed {@link SyncedMessage} to process
 	 */
-	public abstract void onSuccess(SyncedMessage item);
+	public abstract void updateSquashedMessage(SyncedMessage item);
 	
 }
--- app-api/src/main/java/org/openmrs/eip/app/receiver/SearchIndexUpdatingProcessor.java
+++ app-api/src/main/java/org/openmrs/eip/app/receiver/SearchIndexUpdatingProcessor.java
@@ -2,16 +2,9 @@ package org.openmrs.eip.app.receiver;
 
 import static org.openmrs.eip.app.SyncConstants.BEAN_NAME_SYNC_EXECUTOR;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.apache.camel.ProducerTemplate;
-import org.apache.commons.collections.CollectionUtils;
-import org.openmrs.eip.app.AppUtils;
 import org.openmrs.eip.app.management.entity.receiver.SyncedMessage;
 import org.openmrs.eip.app.management.repository.SyncedMessageRepository;
 import org.openmrs.eip.component.SyncProfiles;
@@ -41,76 +34,8 @@ public class SearchIndexUpdatingProcessor extends BaseSendToCamelPostSyncActionP
 	}
 	
 	@Override
-	public String getUniqueId(SyncedMessage item) {
-		return item.getId().toString();
-	}
-	
-	@Override
 	public String getQueueName() {
 		return "search-index-update";
-	}
-	
-	@Override
-	public String getThreadName(SyncedMessage item) {
-		return item.getSite().getIdentifier() + "-" + item.getMessageUuid() + "-"
-		        + AppUtils.getSimpleName(item.getModelClassName()) + "-" + item.getIdentifier();
-	}
-	
-	@Override
-	public String getLogicalType(SyncedMessage item) {
-		//Since we squash msgs for the same entity so need to worry about parallel msg processing for the same entity 
-		return item.getClass().getName();
-	}
-	
-	@Override
-	public List<String> getLogicalTypeHierarchy(String logicalType) {
-		//Since we squash events for the same entity so need to worry about parallel msg processing for the same entity
-		return null;
-	}
-	
-	@Override
-	public void processWork(List<SyncedMessage> items) throws Exception {
-		//Squash events for the same entity to the latest, this ensures exactly one message for the same entity in case 
-		//of multiple in this run in an effort to reduce calls to OpenMRS endpoints
-		Map<String, SyncedMessage> keyAndLatestMsgMap = new LinkedHashMap(items.size());
-		items.stream().forEach(msg -> {
-			String modelClass = msg.getModelClassName();
-			if (ReceiverUtils.isSubclass(modelClass)) {
-				if (log.isTraceEnabled()) {
-					log.trace("Getting parent model class for " + modelClass);
-				}
-				
-				String parentClass = ReceiverUtils.getParentModelClassName(modelClass);
-				
-				if (log.isTraceEnabled()) {
-					log.trace("Parent model class name for " + modelClass + " is " + parentClass);
-				}
-				
-				modelClass = parentClass;
-			}
-			
-			keyAndLatestMsgMap.put(modelClass + "#" + msg.getIdentifier(), msg);
-		});
-		
-		Collection<SyncedMessage> latest = keyAndLatestMsgMap.values();
-		
-		//Update the search index for the latest events for each entity
-		doProcessWork(new ArrayList(latest));
-		
-		Collection<SyncedMessage> skipped = CollectionUtils.subtract(items, latest);
-		skipped.stream().forEach(msg -> msg.setSearchIndexUpdated(true));
-		
-		//Earlier events for each entity get marked as processed in the DB without calling OpenMRS endpoints
-		doProcessWork(new ArrayList(skipped));
-	}
-	
-	protected void doProcessWork(List<SyncedMessage> items) throws Exception {
-		super.processWork(items);
-	}
-	
-	@Override
-	public boolean skipSend(SyncedMessage item) {
-		return item.isSearchIndexUpdated();
 	}
 	
 	@Override
@@ -126,6 +51,16 @@ public class SearchIndexUpdatingProcessor extends BaseSendToCamelPostSyncActionP
 	public Object convertBody(SyncedMessage item) {
 		return ReceiverUtils.generateSearchIndexUpdatePayload(item.getModelClassName(), item.getIdentifier(),
 		    item.getOperation());
+	}
+	
+	@Override
+	public boolean skipSend(SyncedMessage item) {
+		return item.isSearchIndexUpdated();
+	}
+	
+	@Override
+	public void updateSquashedMessage(SyncedMessage item) {
+		item.setSearchIndexUpdated(true);
 	}
 	
 }
