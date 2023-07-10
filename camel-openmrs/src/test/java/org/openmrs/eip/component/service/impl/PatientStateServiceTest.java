d6ea8eab1fb6b08214deedd2aaa3c82427658b6c
wluyima <wyclif@openmrs.org>
2023-07-10 11:34:52 +0300

    EC-470 - Deleted erp work order related code

--- camel-openmrs/src/test/java/org/openmrs/eip/component/service/impl/PatientStateServiceTest.java
+++ camel-openmrs/src/test/java/org/openmrs/eip/component/service/impl/PatientStateServiceTest.java
@@ -5,16 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.openmrs.eip.component.model.PatientStateModel;
-import org.openmrs.eip.component.service.TableToSyncEnum;
 import org.openmrs.eip.component.entity.PatientState;
 import org.openmrs.eip.component.mapper.EntityToModelMapper;
 import org.openmrs.eip.component.mapper.ModelToEntityMapper;
+import org.openmrs.eip.component.model.PatientStateModel;
 import org.openmrs.eip.component.repository.SyncEntityRepository;
+import org.openmrs.eip.component.service.TableToSyncEnum;
 
-import static org.junit.Assert.assertEquals;
-
-public class PatientWorkOrderStateEnumServiceTest {
+public class PatientStateServiceTest {
 	
 	@Mock
 	private SyncEntityRepository<PatientState> repository;
