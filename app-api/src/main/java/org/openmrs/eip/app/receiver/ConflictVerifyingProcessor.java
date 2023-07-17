1052abf1164b552a1c617a3489cad9f9e14b543b
wluyima <wyclif@openmrs.org>
2023-07-17 15:35:20 +0300

    EC-537 - Added a processor to verify conflict queue items

---
+++ app-api/src/main/java/org/openmrs/eip/app/receiver/ConflictVerifyingProcessor.java
@@ -0,0 +1,91 @@
+package org.openmrs.eip.app.receiver;
+
+import static org.openmrs.eip.app.SyncConstants.BEAN_NAME_SYNC_EXECUTOR;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.apache.camel.ProducerTemplate;
+import org.openmrs.eip.app.BasePureParallelQueueProcessor;
+import org.openmrs.eip.app.management.entity.ConflictQueueItem;
+import org.openmrs.eip.app.management.service.ConflictService;
+import org.openmrs.eip.component.SyncProfiles;
+import org.openmrs.eip.component.management.hash.entity.BaseHashEntity;
+import org.openmrs.eip.component.model.BaseModel;
+import org.openmrs.eip.component.service.TableToSyncEnum;
+import org.openmrs.eip.component.service.facade.EntityServiceFacade;
+import org.openmrs.eip.component.utils.HashUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * Processes a ConflictQueueItem by checking if it has a valid hash on file, if the hash is valid it
+ * gets moved to the retry queue for re-syncing otherwise stays in the conflict queue.
+ */
+@Component("conflictVerifyingProcessor")
+@Profile(SyncProfiles.RECEIVER)
+public class ConflictVerifyingProcessor extends BasePureParallelQueueProcessor<ConflictQueueItem> {
+	
+	protected static final Logger log = LoggerFactory.getLogger(ConflictVerifyingProcessor.class);
+	
+	private ConflictService service;
+	
+	private EntityServiceFacade serviceFacade;
+	
+	private ProducerTemplate producerTemplate;
+	
+	public ConflictVerifyingProcessor(@Qualifier(BEAN_NAME_SYNC_EXECUTOR) ThreadPoolExecutor executor,
+	    ConflictService service, EntityServiceFacade serviceFacade, ProducerTemplate producerTemplate) {
+		super(executor);
+		this.service = service;
+		this.serviceFacade = serviceFacade;
+		this.producerTemplate = producerTemplate;
+	}
+	
+	@Override
+	public String getProcessorName() {
+		return "conflict verifier";
+	}
+	
+	@Override
+	public String getQueueName() {
+		return "conflict-verifier";
+	}
+	
+	@Override
+	public String getThreadName(ConflictQueueItem item) {
+		return item.getMessageUuid();
+	}
+	
+	@Override
+	public void processItem(ConflictQueueItem item) {
+		TableToSyncEnum tableToSyncEnum = TableToSyncEnum.getTableToSyncEnumByModelClassName(item.getModelClassName());
+		BaseModel dbModel = serviceFacade.getModel(tableToSyncEnum, item.getIdentifier());
+		boolean move;
+		String reason = null;
+		if (dbModel != null) {
+			Class<? extends BaseHashEntity> hashClass = TableToSyncEnum.getHashClass(dbModel);
+			BaseHashEntity storedHash = HashUtils.getStoredHash(item.getIdentifier(), hashClass, producerTemplate);
+			move = storedHash != null && HashUtils.computeHash(dbModel).equals(storedHash.getHash());
+			if (move) {
+				reason = "Moved from conflict queue because the hash on file is valid";
+			}
+			
+			//TODO If Incoming and existing states match, also move the conflict to retry queue.
+		} else {
+			move = true;
+			reason = "No entity found in the database associated to conflict item with id: " + item.getId();
+		}
+		
+		if (move) {
+			if (log.isDebugEnabled()) {
+				log.debug(reason);
+			}
+			
+			service.moveToRetryQueue(item, reason);
+		}
+	}
+	
+}
---
+++ app-api/src/test/java/org/openmrs/eip/app/receiver/ConflictVerifyingProcessorTest.java
@@ -0,0 +1,134 @@
+package org.openmrs.eip.app.receiver;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.powermock.reflect.Whitebox.setInternalState;
+
+import org.apache.camel.ProducerTemplate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.openmrs.eip.app.BaseQueueProcessor;
+import org.openmrs.eip.app.management.entity.ConflictQueueItem;
+import org.openmrs.eip.app.management.service.ConflictService;
+import org.openmrs.eip.component.management.hash.entity.PersonHash;
+import org.openmrs.eip.component.model.BaseModel;
+import org.openmrs.eip.component.model.PersonModel;
+import org.openmrs.eip.component.service.TableToSyncEnum;
+import org.openmrs.eip.component.service.facade.EntityServiceFacade;
+import org.openmrs.eip.component.utils.HashUtils;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(HashUtils.class)
+public class ConflictVerifyingProcessorTest {
+	
+	private ConflictVerifyingProcessor processor;
+	
+	@Mock
+	private ConflictService mockService;
+	
+	@Mock
+	private EntityServiceFacade mockEntityService;
+	
+	@Mock
+	private ProducerTemplate mockProducerTemplate;
+	
+	@Before
+	public void setup() {
+		PowerMockito.mockStatic(HashUtils.class);
+		Whitebox.setInternalState(BaseQueueProcessor.class, "initialized", true);
+		processor = new ConflictVerifyingProcessor(null, mockService, mockEntityService, mockProducerTemplate);
+	}
+	
+	@After
+	public void tearDown() {
+		setInternalState(BaseQueueProcessor.class, "initialized", false);
+	}
+	
+	@Test
+	public void getThreadName_shouldReturnArchiveMessageUuid() {
+		final String messageUuid = "message-uuid";
+		ConflictQueueItem conflict = new ConflictQueueItem();
+		conflict.setMessageUuid(messageUuid);
+		assertEquals(messageUuid, processor.getThreadName(conflict));
+	}
+	
+	@Test
+	public void processItem_shouldNotMoveTheConflictIfTheHashOnFileIsInvalid() {
+		ConflictQueueItem c = new ConflictQueueItem();
+		c.setModelClassName(PersonModel.class.getName());
+		final Long id = 3L;
+		final String uuid = "test-uuid";
+		c.setId(id);
+		c.setIdentifier(uuid);
+		BaseModel model = new PersonModel();
+		when(mockEntityService.getModel(TableToSyncEnum.PERSON, uuid)).thenReturn(model);
+		PersonHash storedHash = new PersonHash();
+		storedHash.setHash("some hash");
+		when(HashUtils.getStoredHash(uuid, PersonHash.class, mockProducerTemplate)).thenReturn(storedHash);
+		when(HashUtils.computeHash(model)).thenReturn("another hash");
+		
+		processor.processItem(c);
+		
+		Mockito.verifyNoInteractions(mockService);
+	}
+	
+	@Test
+	public void processItem_shouldNotMoveTheConflictIfNoHashIsFoundOnFile() {
+		ConflictQueueItem c = new ConflictQueueItem();
+		c.setModelClassName(PersonModel.class.getName());
+		final Long id = 3L;
+		final String uuid = "test-uuid";
+		c.setId(id);
+		c.setIdentifier(uuid);
+		BaseModel model = new PersonModel();
+		when(mockEntityService.getModel(TableToSyncEnum.PERSON, uuid)).thenReturn(model);
+		
+		processor.processItem(c);
+		
+		Mockito.verifyNoInteractions(mockService);
+	}
+	
+	@Test
+	public void processItem_shouldMoveTheConflictToTheRetryQueueIfTheHashOnFileIsInvalid() {
+		ConflictQueueItem c = new ConflictQueueItem();
+		c.setModelClassName(PersonModel.class.getName());
+		final Long id = 3L;
+		final String uuid = "test-uuid";
+		c.setId(id);
+		c.setIdentifier(uuid);
+		BaseModel model = new PersonModel();
+		when(mockEntityService.getModel(TableToSyncEnum.PERSON, uuid)).thenReturn(model);
+		final String hash = "valid-hash";
+		PersonHash storedHash = new PersonHash();
+		storedHash.setHash(hash);
+		when(HashUtils.getStoredHash(uuid, PersonHash.class, mockProducerTemplate)).thenReturn(storedHash);
+		when(HashUtils.computeHash(model)).thenReturn(hash);
+		
+		processor.processItem(c);
+		
+		final String reason = "Moved from conflict queue because the hash on file is valid";
+		Mockito.verify(mockService).moveToRetryQueue(c, reason);
+	}
+	
+	@Test
+	public void processItem_shouldMoveTheConflictToTheRetryQueueIfNoEntityIsFoundInTheDatabase() {
+		ConflictQueueItem c = new ConflictQueueItem();
+		c.setModelClassName(PersonModel.class.getName());
+		final Long id = 3L;
+		c.setId(id);
+		
+		processor.processItem(c);
+		
+		final String reason = "No entity found in the database associated to conflict item with id: " + id;
+		Mockito.verify(mockService).moveToRetryQueue(c, reason);
+	}
+	
+}
