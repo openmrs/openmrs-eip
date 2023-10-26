6b4bc74243bec953a89e197a4dd9402064497da4
wluyima <wyclif@openmrs.org>
2023-10-26 12:46:03 +0300

    EC-545 - Removed dashboard generator logic from the backend

--- web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverDashboardGenerator.java
+++ web-receiver/src/main/java/org/openmrs/eip/web/receiver/ReceiverDashboardGenerator.java
@@ -1,17 +1,6 @@
 package org.openmrs.eip.web.receiver;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.apache.camel.ProducerTemplate;
-import org.openmrs.eip.app.AppUtils;
-import org.openmrs.eip.app.management.entity.receiver.ConflictQueueItem;
-import org.openmrs.eip.app.management.entity.receiver.ReceiverRetryQueueItem;
-import org.openmrs.eip.app.management.entity.receiver.SyncMessage;
-import org.openmrs.eip.app.management.entity.receiver.SyncedMessage;
-import org.openmrs.eip.component.SyncOperation;
 import org.openmrs.eip.component.SyncProfiles;
 import org.openmrs.eip.web.BaseDashboardGenerator;
 import org.openmrs.eip.web.Dashboard;
@@ -23,26 +12,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile(SyncProfiles.RECEIVER)
 public class ReceiverDashboardGenerator extends BaseDashboardGenerator {
-	
-	private static final String SYNC_ENTITY_NAME = SyncMessage.class.getSimpleName();
-	
-	private static final String SYNCED_ENTITY_NAME = SyncedMessage.class.getSimpleName();
-	
-	private static final String CONFLICT_ENTITY_NAME = ConflictQueueItem.class.getSimpleName();
-	
-	private static final String ERROR_ENTITY_NAME = ReceiverRetryQueueItem.class.getSimpleName();
-	
-	protected static final String KEY_TOTAL_COUNT = "totalCount";
-	
-	protected static final String KEY_ENTITY_STATS = "entityStatsMap";
-	
-	protected static final String KEY_SYNC_MSGS = "syncMsgs";
-	
-	protected static final String KEY_SYNCED_MSGS = "syncedMsgs";
-	
-	protected static final String KEY_ERRORS = "errors";
-	
-	protected static final String KEY_CONFLICTS = "conflicts";
 	
 	@Autowired
 	public ReceiverDashboardGenerator(ProducerTemplate producerTemplate) {
@@ -59,95 +28,7 @@ public class ReceiverDashboardGenerator extends BaseDashboardGenerator {
 	 */
 	@Override
 	public Dashboard generate() {
-		final AtomicInteger totalSyncMsgCount = new AtomicInteger();
-		final Map<String, Map> syncMsgsEntityStatsMap = new ConcurrentHashMap();
-		final AtomicInteger totalSyncedMsgCount = new AtomicInteger();
-		final Map<String, Map> syncedMsgsEntityStatsMap = new ConcurrentHashMap();
-		final AtomicInteger totalErrorCount = new AtomicInteger();
-		final Map<String, Map> errorEntityStatsMap = new ConcurrentHashMap();
-		final AtomicInteger totalConflictCount = new AtomicInteger();
-		final Map<String, Map> conflictEntityStatsMap = new ConcurrentHashMap();
-		
-		AppUtils.getClassAndSimpleNameMap().keySet().parallelStream().forEach(clazz -> {
-			Arrays.stream(SyncOperation.values()).parallel().forEach(op -> {
-				Integer msgCount = getCount(SYNC_ENTITY_NAME, clazz, op);
-				totalSyncMsgCount.addAndGet(msgCount);
-				
-				if (msgCount > 0) {
-					synchronized (this) {
-						if (syncMsgsEntityStatsMap.get(clazz) == null) {
-							syncMsgsEntityStatsMap.put(clazz, new ConcurrentHashMap());
-						}
-					}
-					
-					syncMsgsEntityStatsMap.get(clazz).put(op, msgCount);
-				}
-				
-				Integer syncedMsgCount = getCount(SYNCED_ENTITY_NAME, clazz, op);
-				totalSyncedMsgCount.addAndGet(syncedMsgCount);
-				
-				if (syncedMsgCount > 0) {
-					synchronized (this) {
-						if (syncedMsgsEntityStatsMap.get(clazz) == null) {
-							syncedMsgsEntityStatsMap.put(clazz, new ConcurrentHashMap());
-						}
-					}
-					
-					syncedMsgsEntityStatsMap.get(clazz).put(op, syncedMsgCount);
-				}
-				
-				Integer errorCount = getCount(ERROR_ENTITY_NAME, clazz, op);
-				totalErrorCount.addAndGet(errorCount);
-				
-				if (errorCount > 0) {
-					synchronized (this) {
-						if (errorEntityStatsMap.get(clazz) == null) {
-							errorEntityStatsMap.put(clazz, new ConcurrentHashMap());
-						}
-					}
-					
-					errorEntityStatsMap.get(clazz).put(op, errorCount);
-				}
-				
-				Integer conflictCount = getCount(CONFLICT_ENTITY_NAME, clazz, op);
-				totalConflictCount.addAndGet(conflictCount);
-				
-				if (conflictCount > 0) {
-					synchronized (this) {
-						if (conflictEntityStatsMap.get(clazz) == null) {
-							conflictEntityStatsMap.put(clazz, new ConcurrentHashMap());
-						}
-					}
-					
-					conflictEntityStatsMap.get(clazz).put(op, conflictCount);
-				}
-				
-			});
-		});
-		
-		Map<String, Object> syncMsgs = new ConcurrentHashMap();
-		syncMsgs.put(KEY_TOTAL_COUNT, totalSyncMsgCount);
-		syncMsgs.put(KEY_ENTITY_STATS, syncMsgsEntityStatsMap);
-		
-		Map<String, Object> syncedMsgs = new ConcurrentHashMap();
-		syncedMsgs.put(KEY_TOTAL_COUNT, totalSyncedMsgCount);
-		syncedMsgs.put(KEY_ENTITY_STATS, syncedMsgsEntityStatsMap);
-		
-		Map<String, Object> errors = new ConcurrentHashMap();
-		errors.put(KEY_TOTAL_COUNT, totalErrorCount);
-		errors.put(KEY_ENTITY_STATS, errorEntityStatsMap);
-		
-		Map<String, Object> conflicts = new ConcurrentHashMap();
-		conflicts.put(KEY_TOTAL_COUNT, totalConflictCount);
-		conflicts.put(KEY_ENTITY_STATS, conflictEntityStatsMap);
-		
-		Dashboard dashboard = new Dashboard();
-		dashboard.add(KEY_SYNC_MSGS, syncMsgs);
-		dashboard.add(KEY_SYNCED_MSGS, syncedMsgs);
-		dashboard.add(KEY_ERRORS, errors);
-		dashboard.add(KEY_CONFLICTS, conflicts);
-		
-		return dashboard;
+		return null;
 	}
 	
 }
--- web-receiver/src/test/java/org/openmrs/eip/web/receiver/ReceiverDashboardGeneratorTest.java
+++ web-receiver/src/test/java/org/openmrs/eip/web/receiver/ReceiverDashboardGeneratorTest.java
@@ -1,102 +1,14 @@
 package org.openmrs.eip.web.receiver;
 
-import static org.openmrs.eip.app.SyncConstants.MGT_DATASOURCE_NAME;
-import static org.openmrs.eip.app.SyncConstants.MGT_TX_MGR;
-
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.openmrs.eip.app.receiver.BaseReceiverTest;
-import org.openmrs.eip.component.SyncOperation;
-import org.openmrs.eip.component.model.OrderModel;
-import org.openmrs.eip.component.model.PatientModel;
-import org.openmrs.eip.component.model.PersonModel;
-import org.openmrs.eip.web.Dashboard;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
 
-public class ReceiverDashboardGeneratorTest extends BaseReceiverTest {
-	
-	@Autowired
-	private ReceiverDashboardGenerator generator;
+public class ReceiverDashboardGeneratorTest {
 	
 	@Test
 	public void getCategorizationProperty_shouldReturnModelClassName() {
+		ReceiverDashboardGenerator generator = new ReceiverDashboardGenerator(null);
 		Assert.assertEquals("modelClassName", generator.getCategorizationProperty(null));
-	}
-	
-	@Test
-	@Sql(scripts = { "classpath:mgt_site_info.sql", "classpath:mgt_receiver_sync_msg.sql",
-	        "classpath:mgt_receiver_synced_msg.sql", "classpath:mgt_receiver_retry_queue.sql",
-	        "classpath:mgt_receiver_conflict_queue.sql" }, config = @SqlConfig(dataSource = MGT_DATASOURCE_NAME, transactionManager = MGT_TX_MGR))
-	public void shouldGenerateTheDashboard() {
-		Dashboard dashboard = generator.generate();
-		Map<String, Object> syncMsgs = (Map) dashboard.getEntries().get(ReceiverDashboardGenerator.KEY_SYNC_MSGS);
-		Assert.assertEquals(2, syncMsgs.size());
-		Assert.assertEquals(6, ((AtomicInteger) syncMsgs.get(ReceiverDashboardGenerator.KEY_TOTAL_COUNT)).get());
-		Map<String, Map> entityStatsMap = (Map) syncMsgs.get(ReceiverDashboardGenerator.KEY_ENTITY_STATS);
-		Assert.assertEquals(2, entityStatsMap.size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).size());
-		Assert.assertEquals(4, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.c));
-		Assert.assertEquals(1, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.d));
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).size());
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.c));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.d));
-		
-		Map<String, Object> syncedMsgs = (Map) dashboard.getEntries().get(ReceiverDashboardGenerator.KEY_SYNCED_MSGS);
-		Assert.assertEquals(2, syncedMsgs.size());
-		Assert.assertEquals(35, ((AtomicInteger) syncedMsgs.get(ReceiverDashboardGenerator.KEY_TOTAL_COUNT)).get());
-		entityStatsMap = (Map) syncedMsgs.get(ReceiverDashboardGenerator.KEY_ENTITY_STATS);
-		Assert.assertEquals(2, entityStatsMap.size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.c));
-		Assert.assertEquals(3, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.d));
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).size());
-		Assert.assertEquals(30, entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.c));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.d));
-		
-		Map<String, Object> errors = (Map) dashboard.getEntries().get(ReceiverDashboardGenerator.KEY_ERRORS);
-		Assert.assertEquals(2, errors.size());
-		Assert.assertEquals(5, ((AtomicInteger) errors.get(ReceiverDashboardGenerator.KEY_TOTAL_COUNT)).get());
-		entityStatsMap = (Map) errors.get(ReceiverDashboardGenerator.KEY_ENTITY_STATS);
-		Assert.assertEquals(3, entityStatsMap.size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.c));
-		Assert.assertEquals(1, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.d));
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).size());
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.c));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.d));
-		Assert.assertEquals(1, entityStatsMap.get(OrderModel.class.getName()).size());
-		Assert.assertEquals(1, entityStatsMap.get(OrderModel.class.getName()).get(SyncOperation.c));
-		Assert.assertNull(entityStatsMap.get(OrderModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(OrderModel.class.getName()).get(SyncOperation.d));
-		
-		Map<String, Object> conflicts = (Map) dashboard.getEntries().get(ReceiverDashboardGenerator.KEY_CONFLICTS);
-		Assert.assertEquals(2, conflicts.size());
-		Assert.assertEquals(5, ((AtomicInteger) conflicts.get(ReceiverDashboardGenerator.KEY_TOTAL_COUNT)).get());
-		entityStatsMap = (Map) conflicts.get(ReceiverDashboardGenerator.KEY_ENTITY_STATS);
-		Assert.assertEquals(3, entityStatsMap.size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).size());
-		Assert.assertEquals(2, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.c));
-		Assert.assertEquals(1, entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PersonModel.class.getName()).get(SyncOperation.d));
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).size());
-		Assert.assertEquals(1, entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.c));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(PatientModel.class.getName()).get(SyncOperation.d));
-		Assert.assertEquals(1, entityStatsMap.get(OrderModel.class.getName()).size());
-		Assert.assertEquals(1, entityStatsMap.get(OrderModel.class.getName()).get(SyncOperation.c));
-		Assert.assertNull(entityStatsMap.get(OrderModel.class.getName()).get(SyncOperation.u));
-		Assert.assertNull(entityStatsMap.get(OrderModel.class.getName()).get(SyncOperation.d));
 	}
 	
 }
