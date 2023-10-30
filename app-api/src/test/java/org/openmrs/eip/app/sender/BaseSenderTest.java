2709eea29ffc21a04899e4203a50a0b300431ecc
wluyima <wyclif@openmrs.org>
2023-10-30 13:14:56 +0300

    EC-557 - Fixed failing tests

--- app-api/src/test/java/org/openmrs/eip/app/sender/BaseSenderTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/sender/BaseSenderTest.java
@@ -3,6 +3,8 @@ package org.openmrs.eip.app.sender;
 import org.openmrs.eip.BaseDbBackedCamelTest;
 import org.openmrs.eip.component.SyncProfiles;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles(SyncProfiles.SENDER)
+@TestPropertySource(properties = "debezium.offsetFilename=./target/offset")
 public abstract class BaseSenderTest extends BaseDbBackedCamelTest {}
