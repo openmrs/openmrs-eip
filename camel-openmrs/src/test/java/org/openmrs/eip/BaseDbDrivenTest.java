a1b5b7adda266674a10e5f3051f7179d47939e32
wluyima <wyclif@openmrs.org>
2023-11-30 12:02:23 +0300

    EC-597 - Cleaned up more dependencies, fixed DB backed tests in camel-opemmrs module and

--- camel-openmrs/src/test/java/org/openmrs/eip/BaseDbDrivenTest.java
+++ camel-openmrs/src/test/java/org/openmrs/eip/BaseDbDrivenTest.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.test.spring.CamelSpringRunner;
 import org.junit.runner.RunWith;
 import org.openmrs.eip.component.Constants;
 import org.slf4j.Logger;
@@ -18,13 +17,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.SqlScriptsTestExecutionListener;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(CamelSpringRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = { TestCamelDbConfig.class })
 @Transactional
 @TestExecutionListeners({ DirtiesContextBeforeModesTestExecutionListener.class, MockitoTestExecutionListener.class,
