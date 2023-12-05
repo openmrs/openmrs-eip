102a530f1fc8891aa1ee0bb383979ccd88224988
wluyima <wyclif@openmrs.org>
2023-12-05 10:06:12 +0300

    EC-597 - Added new method to fetch sync messages including tests

--- app-api/src/main/java/org/openmrs/eip/app/management/repository/SyncMessageRepository.java
+++ app-api/src/main/java/org/openmrs/eip/app/management/repository/SyncMessageRepository.java
@@ -2,7 +2,9 @@ package org.openmrs.eip.app.management.repository;
 
 import java.util.List;
 
+import org.openmrs.eip.app.management.entity.receiver.SiteInfo;
 import org.openmrs.eip.app.management.entity.receiver.SyncMessage;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SyncMessageRepository extends JpaRepository<SyncMessage, Long> {
@@ -15,5 +17,14 @@ public interface SyncMessageRepository extends JpaRepository<SyncMessage, Long> 
 	 * @return count of matching sync messages
 	 */
 	long countByIdentifierAndModelClassNameIn(String identifier, List<String> modelClassNames);
+	
+	/**
+	 * Gets a page of sync messages for the specified site
+	 *
+	 * @param site the site to match
+	 * @param pageable {@link Pageable} instance
+	 * @return list of sync messages
+	 */
+	List<SyncMessage> getSyncMessageBySiteOrderByDateCreated(SiteInfo site, Pageable pageable);
 	
 }
--- app-api/src/test/java/org/openmrs/eip/app/management/repository/SyncMessageRepositoryTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/management/repository/SyncMessageRepositoryTest.java
@@ -5,12 +5,17 @@ import static org.junit.Assert.assertEquals;
 import static org.openmrs.eip.app.SyncConstants.MGT_DATASOURCE_NAME;
 import static org.openmrs.eip.app.SyncConstants.MGT_TX_MGR;
 
+import java.util.List;
+
 import org.junit.Test;
+import org.openmrs.eip.app.management.entity.receiver.SiteInfo;
+import org.openmrs.eip.app.management.entity.receiver.SyncMessage;
 import org.openmrs.eip.app.receiver.BaseReceiverTest;
 import org.openmrs.eip.component.model.PatientModel;
 import org.openmrs.eip.component.model.PersonModel;
 import org.openmrs.eip.component.model.VisitModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 
@@ -20,6 +25,9 @@ public class SyncMessageRepositoryTest extends BaseReceiverTest {
 	
 	@Autowired
 	private SyncMessageRepository repo;
+	
+	@Autowired
+	private SiteRepository siteRepo;
 	
 	@Test
 	public void countByIdentifierAndModelClassNameIn_shouldGetTheCountOfMatchingSyncItems() {
@@ -36,6 +44,30 @@ public class SyncMessageRepositoryTest extends BaseReceiverTest {
 	public void countByIdentifierAndModelClassNameIn_shouldReturnZeroIfTheClassNamesHaveNoMatch() {
 		assertEquals(0, repo.countByIdentifierAndModelClassNameIn("4bfd940e-32dc-491f-8038-a8f3afe3e36c",
 		    asList(VisitModel.class.getName())));
+	}
+	
+	@Test
+	public void getSyncMessageBySiteOrderByDateCreated_shouldGetSiteMessagesOrderedByDateCreated() {
+		SiteInfo site = siteRepo.getReferenceById(1L);
+		
+		List<SyncMessage> syncMessages = repo.getSyncMessageBySiteOrderByDateCreated(site, PageRequest.of(0, 1000));
+		
+		assertEquals(3, syncMessages.size());
+		assertEquals(3l, syncMessages.get(0).getId().longValue());
+		assertEquals(1l, syncMessages.get(1).getId().longValue());
+		assertEquals(2l, syncMessages.get(2).getId().longValue());
+	}
+	
+	@Test
+	public void getSyncMessageBySiteOrderByDateCreated_shouldGetAPageOfSiteMessagesOrderedByDateCreated() {
+		final int pageSize = 2;
+		SiteInfo site = siteRepo.getReferenceById(1L);
+		
+		List<SyncMessage> syncMessages = repo.getSyncMessageBySiteOrderByDateCreated(site, PageRequest.of(0, pageSize));
+		
+		assertEquals(pageSize, syncMessages.size());
+		assertEquals(3l, syncMessages.get(0).getId().longValue());
+		assertEquals(1l, syncMessages.get(1).getId().longValue());
 	}
 	
 }
