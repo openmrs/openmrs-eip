df09d804648d227bc295b497a5faccee2b436b1a
wluyima <wyclif@openmrs.org>
2024-01-11 12:14:46 +0300

    EC-619 - Delete debezium event and retry if the entity is not found in the database

--- app-api/src/test/java/org/openmrs/eip/app/route/sender/DbSyncRouteTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/route/sender/DbSyncRouteTest.java
@@ -21,10 +21,13 @@ import org.openmrs.eip.app.management.entity.sender.DebeziumEvent;
 import org.openmrs.eip.app.management.entity.sender.SenderRetryQueueItem;
 import org.openmrs.eip.app.management.entity.sender.SenderSyncMessage;
 import org.openmrs.eip.app.management.entity.sender.SenderSyncMessage.SenderSyncMessageStatus;
+import org.openmrs.eip.app.management.repository.DebeziumEventRepository;
+import org.openmrs.eip.app.management.repository.SenderRetryRepository;
 import org.openmrs.eip.app.route.TestUtils;
 import org.openmrs.eip.component.exception.EIPException;
 import org.openmrs.eip.component.model.PatientModel;
 import org.openmrs.eip.component.model.PersonModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -36,27 +39,35 @@ import ch.qos.logback.classic.Level;
 @Sql(scripts = { "classpath:openmrs_core_data.sql", "classpath:openmrs_patient.sql" })
 public class DbSyncRouteTest extends BaseSenderRouteTest {
 	
+	@Autowired
+	private DebeziumEventRepository eventRepo;
+	
+	@Autowired
+	private SenderRetryRepository retryRepo;
+	
 	@Override
 	public String getTestRouteFilename() {
 		return "db-sync-route";
 	}
 	
 	@Test
-	public void shouldProcessAnInsertEvent() {
+	public void shouldProcessAnInsertEventAndDeleteIt() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "patient";
 		final String uuid = "abfd940e-32dc-491f-8038-a8f3afe3e35b";
 		final String op = "c";
 		final boolean snapshot = true;
-		DebeziumEvent debeziumEvent = createDebeziumEvent(table, null, uuid, op);
+		DebeziumEvent debeziumEvent = createDebeziumEvent(table, "1", uuid, op);
 		debeziumEvent.setDateCreated(new Date());
+		eventRepo.save(debeziumEvent);
 		debeziumEvent.getEvent().setSnapshot(snapshot);
 		exchange.setProperty(EX_PROP_EVENT, debeziumEvent.getEvent());
 		exchange.setProperty(EX_PROP_DBZM_EVENT, debeziumEvent);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(eventRepo.existsById(debeziumEvent.getId()));
 		List<SenderSyncMessage> syncMsgs = TestUtils.getEntities(SenderSyncMessage.class);
 		assertEquals(1, syncMsgs.size());
 		SenderSyncMessage msg = syncMsgs.get(0);
@@ -81,21 +92,23 @@ public class DbSyncRouteTest extends BaseSenderRouteTest {
 	}
 	
 	@Test
-	public void shouldProcessASnapshotReadEvent() {
+	public void shouldProcessASnapshotReadEventAndDeleteIt() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "patient";
 		final String uuid = "abfd940e-32dc-491f-8038-a8f3afe3e35b";
 		final String op = "s";
 		final boolean snapshot = true;
-		DebeziumEvent debeziumEvent = createDebeziumEvent(table, null, uuid, op);
+		DebeziumEvent debeziumEvent = createDebeziumEvent(table, "1", uuid, op);
 		debeziumEvent.setDateCreated(new Date());
+		eventRepo.save(debeziumEvent);
 		debeziumEvent.getEvent().setSnapshot(snapshot);
 		exchange.setProperty(EX_PROP_EVENT, debeziumEvent.getEvent());
 		exchange.setProperty(EX_PROP_DBZM_EVENT, debeziumEvent);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(eventRepo.existsById(debeziumEvent.getId()));
 		List<SenderSyncMessage> syncMsgs = TestUtils.getEntities(SenderSyncMessage.class);
 		assertEquals(1, syncMsgs.size());
 		SenderSyncMessage msg = syncMsgs.get(0);
@@ -120,19 +133,21 @@ public class DbSyncRouteTest extends BaseSenderRouteTest {
 	}
 	
 	@Test
-	public void shouldProcessAnUpdateEvent() {
+	public void shouldProcessAnUpdateEventAndDeleteIt() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "patient";
 		final String uuid = "abfd940e-32dc-491f-8038-a8f3afe3e35b";
 		final String op = "u";
-		DebeziumEvent debeziumEvent = createDebeziumEvent(table, null, uuid, op);
+		DebeziumEvent debeziumEvent = createDebeziumEvent(table, "1", uuid, op);
 		debeziumEvent.setDateCreated(new Date());
+		eventRepo.save(debeziumEvent);
 		exchange.setProperty(EX_PROP_EVENT, debeziumEvent.getEvent());
 		exchange.setProperty(EX_PROP_DBZM_EVENT, debeziumEvent);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(eventRepo.existsById(debeziumEvent.getId()));
 		List<SenderSyncMessage> syncMsgs = TestUtils.getEntities(SenderSyncMessage.class);
 		assertEquals(1, syncMsgs.size());
 		SenderSyncMessage msg = syncMsgs.get(0);
@@ -157,19 +172,21 @@ public class DbSyncRouteTest extends BaseSenderRouteTest {
 	}
 	
 	@Test
-	public void shouldProcessADeleteEvent() {
+	public void shouldProcessADeleteEventAndDeleteIt() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "person";
 		final String uuid = "person-uuid";
 		final String op = "d";
-		DebeziumEvent debeziumEvent = createDebeziumEvent(table, null, uuid, op);
+		DebeziumEvent debeziumEvent = createDebeziumEvent(table, "1", uuid, op);
 		debeziumEvent.setDateCreated(new Date());
+		eventRepo.save(debeziumEvent);
 		exchange.setProperty(EX_PROP_EVENT, debeziumEvent.getEvent());
 		exchange.setProperty(EX_PROP_DBZM_EVENT, debeziumEvent);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(eventRepo.existsById(debeziumEvent.getId()));
 		List<SenderSyncMessage> syncMsgs = TestUtils.getEntities(SenderSyncMessage.class);
 		assertEquals(1, syncMsgs.size());
 		SenderSyncMessage msg = syncMsgs.get(0);
@@ -194,7 +211,7 @@ public class DbSyncRouteTest extends BaseSenderRouteTest {
 	}
 	
 	@Test
-	public void shouldProcessARetryItem() {
+	public void shouldProcessARetryItemAndDeleteIt() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "patient";
@@ -206,13 +223,14 @@ public class DbSyncRouteTest extends BaseSenderRouteTest {
 		retry.setAttemptCount(1);
 		retry.setExceptionType(EIPException.class.getName());
 		retry.setEventDate(new Date());
-		TestUtils.saveEntity(retry);
+		retryRepo.save(retry);
 		assertEquals(1, getEntities(SenderRetryQueueItem.class).size());
 		exchange.setProperty(EX_PROP_EVENT, retry.getEvent());
 		exchange.setProperty(EX_PROP_RETRY_ITEM, retry);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(retryRepo.existsById(retry.getId()));
 		List<SenderSyncMessage> syncMsgs = TestUtils.getEntities(SenderSyncMessage.class);
 		assertEquals(1, syncMsgs.size());
 		SenderSyncMessage msg = syncMsgs.get(0);
@@ -237,35 +255,70 @@ public class DbSyncRouteTest extends BaseSenderRouteTest {
 	}
 	
 	@Test
-	public void shouldProcessAnEventAndTheEntityIsNotFound() {
+	public void shouldProcessAnEventAndDeleteItIfTheEntityIsNotFound() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "person";
 		final String uuid = "person-uuid";
 		final String op = "u";
-		exchange.setProperty(EX_PROP_EVENT, createEvent(table, null, uuid, op));
+		final boolean snapshot = true;
+		DebeziumEvent debeziumEvent = createDebeziumEvent(table, "1", uuid, op);
+		debeziumEvent.setDateCreated(new Date());
+		eventRepo.save(debeziumEvent);
+		debeziumEvent.getEvent().setSnapshot(snapshot);
+		exchange.setProperty(EX_PROP_EVENT, debeziumEvent.getEvent());
+		exchange.setProperty(EX_PROP_DBZM_EVENT, debeziumEvent);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(eventRepo.existsById(debeziumEvent.getId()));
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		assertMessageLogged(Level.INFO,
 		    "No entity found in the database matching identifier " + uuid + " in table " + table);
 	}
 	
 	@Test
-	public void shouldProcessASyncRequest() {
+	public void shouldProcessARetryAndDeleteItIfTheEntityIsNotFound() {
+		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
+		DefaultExchange exchange = new DefaultExchange(camelContext);
+		final String table = "person";
+		final String uuid = "person-uuid";
+		final String op = "u";
+		SenderRetryQueueItem retry = new SenderRetryQueueItem();
+		retry.setEvent(createEvent(table, "101", uuid, op));
+		retry.setDateCreated(new Date());
+		retry.setAttemptCount(1);
+		retry.setExceptionType(EIPException.class.getName());
+		retry.setEventDate(new Date());
+		retryRepo.save(retry);
+		assertEquals(1, getEntities(SenderRetryQueueItem.class).size());
+		exchange.setProperty(EX_PROP_EVENT, retry.getEvent());
+		exchange.setProperty(EX_PROP_RETRY_ITEM, retry);
+		
+		producerTemplate.send(URI_DBSYNC, exchange);
+		
+		assertFalse(retryRepo.existsById(retry.getId()));
+		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
+		assertMessageLogged(Level.INFO,
+		    "No entity found in the database matching identifier " + uuid + " in table " + table);
+	}
+	
+	@Test
+	public void shouldProcessASyncRequestAndDeleteIt() {
 		assertTrue(TestUtils.getEntities(SenderSyncMessage.class).isEmpty());
 		DefaultExchange exchange = new DefaultExchange(camelContext);
 		final String table = "patient";
 		final String uuid = "abfd940e-32dc-491f-8038-a8f3afe3e35b";
 		final String op = "r";
-		DebeziumEvent debeziumEvent = createDebeziumEvent(table, null, uuid, op);
+		DebeziumEvent debeziumEvent = createDebeziumEvent(table, "1", uuid, op);
 		debeziumEvent.setDateCreated(new Date());
+		eventRepo.save(debeziumEvent);
 		exchange.setProperty(EX_PROP_EVENT, debeziumEvent.getEvent());
 		exchange.setProperty(EX_PROP_DBZM_EVENT, debeziumEvent);
 		
 		producerTemplate.send(URI_DBSYNC, exchange);
 		
+		assertFalse(eventRepo.existsById(debeziumEvent.getId()));
 		List<SenderSyncMessage> syncMsgs = TestUtils.getEntities(SenderSyncMessage.class);
 		assertEquals(1, syncMsgs.size());
 		SenderSyncMessage msg = syncMsgs.get(0);
