b39b03092ce8a89706a0b2d3dd442540005d4705
wluyima <wyclif@openmrs.org>
2024-01-31 14:20:03 +0300

    EC-621 - Added method to get a site by identifier

--- app-api/src/main/java/org/openmrs/eip/app/management/repository/SiteRepository.java
+++ app-api/src/main/java/org/openmrs/eip/app/management/repository/SiteRepository.java
@@ -3,4 +3,14 @@ package org.openmrs.eip.app.management.repository;
 import org.openmrs.eip.app.management.entity.receiver.SiteInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SiteRepository extends JpaRepository<SiteInfo, Long> {}
+public interface SiteRepository extends JpaRepository<SiteInfo, Long> {
+	
+	/**
+	 * Gets a site matching the specified identifier.
+	 * 
+	 * @param identifier the identifier to match
+	 * @return site
+	 */
+	SiteInfo getByIdentifier(String identifier);
+	
+}
