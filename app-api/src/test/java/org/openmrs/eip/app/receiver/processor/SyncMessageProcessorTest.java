f2a6714397d02c4a367c7f1a60c77907182bb13c
wluyima <wyclif@openmrs.org>
2024-03-20 15:30:24 +0300

    EC-477 - Added tests to ensure SyncMessageProcessor processes a message

--- app-api/src/test/java/org/openmrs/eip/app/receiver/processor/SyncMessageProcessorTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/receiver/processor/SyncMessageProcessorTest.java
@@ -5,29 +5,56 @@ import static org.junit.Assert.assertEquals;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
-import org.junit.Assert;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.openmrs.eip.app.AppUtils;
 import org.openmrs.eip.app.BaseQueueProcessor;
 import org.openmrs.eip.app.management.entity.receiver.SiteInfo;
 import org.openmrs.eip.app.management.entity.receiver.SyncMessage;
+import org.openmrs.eip.app.management.service.ReceiverService;
+import org.openmrs.eip.app.receiver.SyncHelper;
 import org.openmrs.eip.component.model.PatientModel;
 import org.openmrs.eip.component.model.PersonModel;
+import org.openmrs.eip.component.model.SyncModel;
 import org.openmrs.eip.component.model.VisitModel;
+import org.openmrs.eip.component.utils.JsonUtils;
 import org.openmrs.eip.component.utils.Utils;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(JsonUtils.class)
 public class SyncMessageProcessorTest {
 	
 	private static final ThreadPoolExecutor EXECUTOR = (ThreadPoolExecutor) Executors.newFixedThreadPool(1);
+	
+	@Mock
+	private SyncHelper mockHelper;
+	
+	@Mock
+	private ReceiverService mockService;
+	
+	@Mock
+	private SyncModel mockSyncModel;
 	
 	private SyncMessageProcessor processor;
 	
 	@Before
 	public void setup() {
+		PowerMockito.mockStatic(JsonUtils.class);
 		Whitebox.setInternalState(BaseQueueProcessor.class, "initialized", true);
-		processor = new SyncMessageProcessor(EXECUTOR, null, null);
+		processor = new SyncMessageProcessor(EXECUTOR, mockService, mockHelper);
+	}
+	
+	@After
+	public void tearDown() {
+		Whitebox.setInternalState(BaseQueueProcessor.class, "initialized", false);
 	}
 	
 	@Test
@@ -70,8 +97,16 @@ public class SyncMessageProcessorTest {
 	}
 	
 	@Test
-	public void processItem_should() {
-		Assert.fail("Add tests");
+	public void processItem_shouldSyncTheMessage() {
+		final String payload = "{}";
+		SyncMessage msg = new SyncMessage();
+		msg.setModelClassName(VisitModel.class.getName());
+		msg.setEntityPayload(payload);
+		Mockito.when(JsonUtils.unmarshalSyncModel(payload)).thenReturn(mockSyncModel);
+		
+		processor.processItem(msg);
+		
+		Mockito.verify(mockHelper).sync(mockSyncModel, false);
 	}
 	
 }
