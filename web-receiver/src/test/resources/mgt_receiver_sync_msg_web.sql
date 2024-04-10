833668190bab55cc874bc39e9dcacb42ae4ea1d7
wluyima <wyclif@openmrs.org>
2024-04-10 14:11:01 -0500

    EC-659 - Fixed another failing test

--- web-receiver/src/test/resources/mgt_receiver_sync_msg_web.sql
+++ web-receiver/src/test/resources/mgt_receiver_sync_msg_web.sql
@@ -1,5 +1,5 @@
-INSERT INTO receiver_sync_msg (id, model_class_name, identifier, operation, message_uuid, site_id, entity_payload, is_snapshot, date_sent_by_sender, date_created)
-VALUES (101, 'org.openmrs.eip.component.model.PersonModel', '1bfd940e-32dc-491f-8038-a8f3afe3e36c', 'c', '16beb8bd-287c-57f2-9786-a7b98c933c04', 1, '{}', 0, '2022-11-07 00:00:00', '2022-11-07 00:00:00.001'),
-       (102, 'org.openmrs.eip.component.model.PersonModel', '1bfd940e-32dc-491f-8038-a8f3afe3e36c', 'u', '26beb8bd-287c-57f2-9786-a7b98c933c04', 1, '{}', 0, '2022-11-07 00:00:00', '2022-11-07 00:00:00.002'),
-       (103, 'org.openmrs.eip.component.model.PersonModel', '2bfd940e-32dc-491f-8038-a8f3afe3e36c', 'c', '36beb8bd-287c-57f2-9786-a7b98c933c04', 1, '{}', 0, '2022-11-07 00:00:00', '2022-11-07 01:00:00.000'),
-       (104, 'org.openmrs.eip.component.model.VisitModel', '3bfd940e-32dc-491f-8038-a8f3afe3e36c', 'c', '46beb8bd-287c-57f2-9786-a7b98c933c04', 2, '{}', 0, '2022-11-07 00:00:00', '2020-08-22 01:00:00.000');
+INSERT INTO receiver_sync_msg (id, model_class_name, identifier, operation, message_uuid, site_id, entity_payload, is_snapshot, date_sent_by_sender, date_received, date_created)
+VALUES (101, 'org.openmrs.eip.component.model.PersonModel', '1bfd940e-32dc-491f-8038-a8f3afe3e36c', 'c', '16beb8bd-287c-57f2-9786-a7b98c933c04', 1, '{}', 0, '2022-11-07 00:00:00', '2022-11-07 00:00:00.001', '2022-11-07 00:00:01.000'),
+       (102, 'org.openmrs.eip.component.model.PersonModel', '1bfd940e-32dc-491f-8038-a8f3afe3e36c', 'u', '26beb8bd-287c-57f2-9786-a7b98c933c04', 1, '{}', 0, '2022-11-07 00:00:00', '2022-11-07 00:00:00.002', '2022-11-07 00:00:01.000'),
+       (103, 'org.openmrs.eip.component.model.PersonModel', '2bfd940e-32dc-491f-8038-a8f3afe3e36c', 'c', '36beb8bd-287c-57f2-9786-a7b98c933c04', 1, '{}', 0, '2022-11-07 00:00:00', '2022-11-07 01:00:00.000', '2022-11-07 01:00:01.000'),
+       (104, 'org.openmrs.eip.component.model.VisitModel', '3bfd940e-32dc-491f-8038-a8f3afe3e36c', 'c', '46beb8bd-287c-57f2-9786-a7b98c933c04', 2, '{}', 0, '2022-11-07 00:00:00', '2020-08-22 01:00:00.000', '2020-08-22 01:00:01.000');
