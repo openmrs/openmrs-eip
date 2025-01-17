f4770078660d994e3631a301f5dd924f5951eb85
Siddharth Vaish <sidvaish97@gmail.com>
2025-01-17 15:09:03 +0530

    Disabled unit tests for Supply and Procedure orders

--- camel-openmrs-fhir/src/main/java/org/openmrs/eip/fhir/routes/resources/SupplyRequestRouter.java
+++ camel-openmrs-fhir/src/main/java/org/openmrs/eip/fhir/routes/resources/SupplyRequestRouter.java
@@ -44,9 +44,7 @@ public class SupplyRequestRouter extends BaseFhirResourceRouter {
 			        String base64Auth = getEncoder().encodeToString(auth.getBytes());
 			        exchange.getIn().setHeader("Authorization", "Basic " + base64Auth);
 		        }).setHeader("CamelHttpMethod", constant("GET"))
-		        //TODO: Replace with {{openmrs.baseUrl}}
-		        .toD("http://openmrs:8080/openmrs/ws/rest/v1/order/${exchangeProperty.event.identifier}")
-		        .process(exchange -> {
+		        .toD("{{openmrs.baseUrl}}/ws/rest/v1/order/${exchangeProperty.event.identifier}").process(exchange -> {
 			        Order order = objectMapper.readValue(exchange.getIn().getBody(String.class), Order.class);
 			        exchange.getMessage().setBody(mapOrderToSupplyRequest(order));
 		        }).setHeader(HEADER_FHIR_EVENT_TYPE, simple("${exchangeProperty." + PROP_EVENT_OPERATION + "}"))
